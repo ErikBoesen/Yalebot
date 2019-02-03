1eee8c62a0a76193583cb53b1e498894527e3672
ErikBoesen <me@erikboesen.com>
2019-02-03 09:43:49 -0500

    Remove image sending code

--- bot.py
+++ bot.py
@@ -24,14 +24,12 @@ def webhook():
         matches = F_PATTERN.match(message["text"])
         if matches is not None and len(matches.groups()):
             reply(matches.groups()[0] + ' ' + SUFFIX)
-        if message["text"] == "YO":
-            reply("", image="https://i.groupme.com/1200x1500.jpeg.de46caa3987c440ea9f4c0b1513278d3.large")
     if message["system"]:
         print("System message!")
 
     return "ok", 200
 
-def reply(text, image=None):
+def reply(text):
     """
     Reply in chat.
     """
@@ -40,9 +38,6 @@ def reply(text, image=None):
         "bot_id": os.environ["BOT_ID"],
         "text": text,
     }
-    if image is not None:
-        data["attachments"] = [{"type": "image",
-                                "url": image}]
     request = Request(url, urlencode(data).encode())
     response = urlopen(request).read().decode()
     print("Response after message send: %s" % response)
