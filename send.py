ab152d077c7c5570dc56e26543b2862204bb0444
ErikBoesen <me@erikboesen.com>
2019-02-02 23:24:30 -0500

    Fix environment variable getting in send.py

--- send.py
+++ send.py
@@ -2,5 +2,5 @@ import requests
 import os
 
 text = input('Message: ')
-requests.post("https://api.groupme.com/v3/bots/post", data={"text": text, "bot_id": os.env["BOT_ID"]})
+requests.post("https://api.groupme.com/v3/bots/post", data={"text": text, "bot_id": os.environ["BOT_ID"]})
 
