d7007a13d655ee3e74ab15376d9d95ef9532ef66
ErikBoesen <me@erikboesen.com>
2019-02-04 10:11:02 -0500

    Add bulldog module

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,3 +2,4 @@ from .countdown import Countdown
 from .zalgo import Zalgo
 from .flip import Flip
 from .vet import Vet
+from .bulldog import Bulldog
---
+++ modules/bulldog.py
@@ -0,0 +1,17 @@
+BOOLA_BOOLA = """Bulldog!  Bulldog!
+Bow, wow, wow
+Eli Yale
+Bulldog!  Bulldog!
+Bow, wow, wow
+Our team can never fail
+
+When the sons of Eli
+Break through the line
+That is the sign we hail
+Bulldog!  Bulldog!
+Bow, wow, wow
+Eli Yale!"""
+
+class Bulldog:
+    def response(self, query):
+        return BOOLA_BOOLA
