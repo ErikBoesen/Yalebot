425fbe4ac3b1b029de9b1680892041fab64dea95
ErikBoesen <me@erikboesen.com>
2019-02-04 09:29:05 -0500

    Support multiple events

--- modules/countdown.py
+++ modules/countdown.py
@@ -1,8 +1,17 @@
 import datetime
 
-BDD_TIME = datetime.datetime(year=2019, month=4, day=15)
+
+class Event:
+    def __init__(self, name: str, date, duration):
+        self.name = name
+        self.date = date
+        self.duration = duration
 
 class Countdown:
+    events = [
+        Event("Bulldog Days", datetime.datetime(year=2019, month=4, day=15), datetime.timedelta(days=3)),
+    ]
+
     def __init__(self):
         pass
 
@@ -11,13 +20,24 @@ class Countdown:
         Calculate response given input.
         :param query: text input to command.
         """
-        return 'There are %d weeks, %d days, %d hours, %d minutes, and %d seconds left until Bulldog Days.' % (self.time())
+        return "There are %d weeks, %d days, %d hours, %d minutes, and %d seconds left until Bulldog Days." % (self.time())
+
+    def next_event(self, now: datetime.datetime) -> Event:
+        """
+        :return: next event in list.
+        """
+        for event in self.events:
+            if event.date + event.duration - now > 0:
+                return event
+        return None
 
     def time(self):
         """
         Get time split into units until Bulldog Days.
         """
-        delta = BDD_TIME - datetime.datetime.now()
+        now = datetime.datetime.now()
+        event = self.next_event(now)
+        delta = event - now
         seconds = delta.total_seconds()
         weeks, seconds = divmod(seconds, 60*60*24*7)
         days, seconds = divmod(seconds, 60*60*24)
