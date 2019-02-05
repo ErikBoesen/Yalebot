ab7f92e43d8ba73c6a9cbc92ae79852e5d819914
ErikBoesen <me@erikboesen.com>
2019-02-04 20:13:36 -0500

    Add testing server

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 GROUPS = {
     46649296: {"name": "Main Yale chat", "bot_id": "1520c98b3da635c8c6383951a6"},
     47743475: {"name": "The loud minority", "bot_id": "004330a9e1f501c837041763fc"},
+    46649296: {"name": "Yalebot testing server", "bot_id": "1520c98b3da635c8c6383951a6"},
 }
 simple_responses = {
     "ping": "Pong!",
