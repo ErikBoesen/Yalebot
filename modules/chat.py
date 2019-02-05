3caa1fa3706f8038050f73f777d57dc9086a2da7
ErikBoesen <me@erikboesen.com>
2019-02-04 20:06:53 -0500

    Reimplement Cleverbot API wrapper myself

--- modules/chat.py
+++ modules/chat.py
@@ -1,9 +1,41 @@
-import clever
 import os
+import requests
+import json
+
+
+class CleverBot(object):
+    def __init__(self, user, key, nick=None):
+        self.user = user
+        self.key = key
+        self.nick = nick
+
+        body = {
+            'user': user,
+            'key': key,
+            'nick': nick
+        }
+        requests.post('https://cleverbot.io/1.0/create', json=body)
+
+    def query(self, text):
+        body = {
+            'user': self.user,
+            'key': self.key,
+            'nick': self.nick,
+            'text': text
+        }
+
+        r = requests.post('https://cleverbot.io/1.0/ask', json=body)
+        r = json.loads(r.text)
+
+        if r['status'] == 'success':
+            return r['response']
+        else:
+            return False
+
 
 class Chat:
     def __init__(self):
-        self.client = clever.CleverBot(user=os.environ["CLEVERBOT_USER"], key=os.environ["CLEVERBOT_KEY"])
+        self.client = CleverBot(user=os.environ["CLEVERBOT_USER"], key=os.environ["CLEVERBOT_KEY"])
 
     def response(self, query):
         return self.client.query(query)
