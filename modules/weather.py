e44685bda483d4552195b06c38e0865439cd8f5a
ErikBoesen <me@erikboesen.com>
2019-02-05 12:53:46 -0500

    Clean up naming in weather module

--- modules/weather.py
+++ modules/weather.py
@@ -3,14 +3,13 @@ import requests
 import os
 
 class Weather(Module):
-    location_name = "New Haven, CT"
-    location_coords = {
+    NH_COORDINATES = {
         "x": 41.3083,
         "y": -72.9279
     }
 
     def response(self, query):
-        r = requests.get("https://api.weather.gov/points/{x},{y}/forecast".format(x=self.location_coords["x"],
-                                                                                  y=self.location_coords["y"]))
-        current_weather = r.json()['properties']['periods'][0]['detailedForecast']
-        return 'Current weather in New Haven: ' + current_weather
+        r = requests.get("https://api.weather.gov/points/{x},{y}/forecast".format(x=self.NH_COORDINATES["x"],
+                                                                                  y=self.NH_COORDINATES["y"]))
+        forecast = r.json()['properties']['periods'][0]['detailedForecast']
+        return 'Current weather in New Haven: ' + forecast
