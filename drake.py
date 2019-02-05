20581cc0f47563e7c0edd6db44c6e5b368bfbcbd
ErikBoesen <me@erikboesen.com>
2019-02-04 20:00:25 -0500

    Get meme font

--- drake.py
+++ drake.py
@@ -1,4 +1,9 @@
-from PIL import Image
+from PIL import Image, ImageFont
 im = Image.open("resources/memes/drake.jpg")
-im.rotate(45).show()
+font = ImageFont.truetype("resources/Lato-Regular.ttf", 16)
+
+left_border = 330
+right_border = 620
+
+im.show()
 
