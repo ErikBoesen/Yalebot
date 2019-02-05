7e21f43084ab6d64a7af9218a7de71d1f3662072
ErikBoesen <me@erikboesen.com>
2019-02-05 12:08:43 -0500

    Use format for weather URL building

--- modules/weather.py
+++ modules/weather.py
@@ -11,7 +11,9 @@ class Weather(Module):
     request_params = {
         "token": os.environ["WEATHER_KEY"]
     }
+
     def response(self, query):
-        weather_response = requests.get('https://api.weather.gov/points/' + location_coords['x'] + ',' + location_coords['y'] + '/forecast').json()
-        current_weather = weather_response['properties']['periods'][0]['detailedForecast']
+        r = requests.get("https://api.weather.gov/points/{x},{y}/forecast".format(x=self.location_coords["x"],
+                                                                                  y=self.location_coords["y"])
+        current_weather = r.json()['properties']['periods'][0]['detailedForecast']
         return current_weather
