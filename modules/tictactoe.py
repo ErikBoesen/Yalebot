cf2819131e38a7f51c08d5c40c373b7f8af3de85
ErikBoesen <me@erikboesen.com>
2019-02-05 21:52:18 -0500

    Work on Tic Tac Toe game

--- modules/tictactoe.py
+++ modules/tictactoe.py
@@ -29,41 +29,88 @@ class TicTacToe(Game):
         board_template = "%c | %c | %c\n--+---+--\n%c | %c | %c\n--+---+--\n%c | %c | %c"
         board_items = [item for sublist in self.board for item in sublist]
         board_items = [self.value_to_char(item) for item in board_items]
-        print(board_template % tuple(board_items))
+        return board_template % tuple(board_items)
 
     def start_game(self, players):
         self.started = True
-        self.board = [[None] * self.RES] * self.RES
+        self.board = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None]
+        ]
         self.render_board()
         self.turn = 0
         self.players = players
-        return "Tic-Tac-Toe game started between {player1} & {player2}! It is {player1}'s turn (X) first.".format(player1=players[0],
-                                                                                                                  player2=players[1])
-
-    def cancel_game(self):
+        return "Tic-Tac-Toe game started between {player1} & {player2}! It is {player1}'s turn (X) first.\n{board}".format(player1=players[0],
+                                                                                                                           player2=players[1],
+                                                                                                                           board=self.render_board())
+    def end_game(self):
         self.started = False
         self.board = None
         self.players = []
+
+    def cancel_game(self):
+        self.end_game()
         return "Tic-Tac-Toe game cancelled."
 
-    def turn(self):
-        pass
+    def place(self, x, y):
+        self.board[y][x] = self.turn
+        if self.check_win():
+            self.end_game()
+            return self.render_board() + "\n" + self.players[self.turn] + " wins!"
+        self.turn = (1, 0)[self.turn]
+        return self.render_board() + "\n" + ("It is now %s's turn" % self.players[self.turn])
+
+    def parse_position(self, string):
+        """
+        Given a word describing a position on the board, give the X or Y value to which that word corresponds.
+        :param string: single word, such as "top", "middle", or "right".
+        """
+        if string in ("top", "left"):
+            return 0
+        elif string in ("middle", "center"):
+            return 1
+        elif string in ("bottom", "right"):
+            return 2
+        else:
+            return None
+
+    def check_win(self):
+        if self.board[0][0] == self.board[0][1] == self.board[0][2] or \
+                self.board[1][0] == self.board[1][0] == self.board[1][2] or \
+                self.board[2][0] == self.board[2][1] == self.board[2][2] or \
+                self.board[0][0] == self.board[1][0] == self.board[2][0] or \
+                self.board[0][1] == self.board[1][1] == self.board[2][1] or \
+                self.board[0][2] == self.board[1][2] == self.board[2][2] or \
+                self.board[0][0] == self.board[1][1] == self.board[2][2] or \
+                self.board[0][2] == self.board[1][1] == self.board[2][0]:
+            return True
+        return False
 
     def response(self, query, name):
         #super().response(query)
         parameters = query.split(" ")
-        if parameters[0] == "start":
+        command = parameters.pop(0)
+        if command == "start":
             if self.started:
                 return "A game is already in progress!"
+            elif len(parameters) == 0:
+                return "Please specify your opponent."
             else:
-                return self.start_game(players=[name, " ".join(parameters[1:])[1:]])
-        elif parameters[0] == "cancel":
+                return self.start_game(players=[name, " ".join(parameters)[1:]])
+        elif command == "cancel":
             if self.started:
                 return self.cancel_game()
             else:
                 return "No game is in progress."
+        elif command == "place":
+            if name != self.players[self.turn]:
+                return "You didn't really think I wouldn't have checked that you're the right person? Try harder, fool. It's %s's turn." % self.players[self.turn]
+            x = self.parse_position(parameters.pop(0))
+            y = self.parse_position(parameters.pop(0))
+            return self.place(x, y)
         else:
-            return "Invalid command."
+            return "Invalid action."
 
 if __name__ == "__main__":
     ttt = TicTacToe()
