76f6e8434fb42b1f152be897bedc68772b203812
ErikBoesen <me@erikboesen.com>
2019-02-06 11:13:57 -0500

    Clean up !help output

--- bot.py
+++ bot.py
@@ -66,11 +66,13 @@ def webhook():
                 #reply("@" + message["name"], group_id, image=meme_commands[command].response(query))
                 reply(meme_commands[command].response(query), group_id)
             elif command == "help":
-                help_string = "--- Yalebot Commands ---\n"
+                help_string = "--- Yalebot Help ---\n\n"
                 help_string += "Simple commands: " + ", ".join(["!" + title for title in simple_responses])
+                help_string += "\n\n"
+                help_string += "Tools:\n"
                 for title in commands:
-                    help_string += "!" + title + ": " + commands[title].DESCRIPTION + "\n"
-                help_string += "Memes: " + ", ".join(["!" + title for title in meme_commands])
+                    help_string += "- !" + title + ": " + commands[title].DESCRIPTION + "\n"
+                help_string += "\nMemes: " + ", ".join(["!" + title for title in meme_commands])
                 reply(help_string, group_id)
             else:
                 reply("Command not found.", group_id)
