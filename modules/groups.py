41cb55cbb3e80e4b063e1adf83f9f03cfbb98e4b
ErikBoesen <me@erikboesen.com>
2019-02-07 08:11:19 -0500

    Add UNC Chapel Hill link

--- modules/groups.py
+++ modules/groups.py
@@ -4,6 +4,7 @@ class Groups(Module):
     DESCRIPTION = "Get links to other admit groups"
     links = {
         "University of Michigan": "46781389/hZehS1",
+        "UNC Chapel Hill": "47739618/Ubl6Cn",
     }
     def response(self, query):
         return '\n'.join(["%s -> https://groupme.com/join_group/%s" % (name, self.links[name]) for name in self.links])
