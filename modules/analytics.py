d112872bb54a291e61cfeccff9d1d3a5abfc1532
ErikBoesen <me@erikboesen.com>
2019-02-07 22:09:48 -0500

    Remove most unnecessary content from logging

--- modules/analytics.py
+++ modules/analytics.py
@@ -37,17 +37,13 @@ class Analytics:
         self.display_data(self.users)
 
     def new_user(self, name):
-        return {"name": name, "messages": 0, "likes": 0, "average_likes_per_message": 0.0, "average_likes_received": 0.0}
+        return {"name": name, "messages": 0, "likes": 0, "likes_received": 0.0}
 
     def populate_users(self, members):
         for member in members:
             user_id = member['user_id']
             name = member['name']  # TODO: May need to be nickname not name
             user_dictionary[user_id] = self.new_user(nickname)
-            # TODO: This is a terrible system and will be removed once it no longer needs to be referenced.
-            # [0] = nickname, [1] = total messages sent in group, like count, [2] = likes per message,
-            # [3] = average likes received per message, [4] = total words sent, [5] = dictionary of likes received from each member
-            # [6] = dictionary of shared likes, [7] = total likes given
 
     def analyze_group(self, GROUP_ID, self.users, message_count):
         message_id = 0
@@ -92,60 +88,10 @@ class Analytics:
 
     def display_data(self, self.users):
         for key in self.users:
-            print(self.users[key][0] + ' Data:')
-            print('Messages Sent: ' + str(self.users[key][1]))
-            print('Total Likes Given: ' + str(self.users[key][7]))
-            try:
-                print('Self Likes: ' + str(self.users[key][5][key]))
-                self_likes = self.users[key][5][key]
-            except KeyError:
-                self_likes = 0
-                print('Self Likes: ' + str(0))
-            print('Total Likes Received: ' + str(self.users[key][2]))
-            print('Average Likes Received Per Message: ' + str(self.users[key][3]))
-
-            print('Total Likes Received with Self Likes Subtracted: ' +
-                  str(self.users[key][2] - self_likes))
-
-            total_likes_minus_self_likes = self.users[key][2] - self_likes
-            try:
-                new_avg = total_likes_minus_self_likes / self.users[key][1]
-            except ZeroDivisionError:  # for the case where the user has sent 0 messages
-                new_avg = 0
-            print('Average Likes Received Per Message with Self Likes Subtracted: '
-                  + str(new_avg))
-
-            print('Total Words Sent: ' + str(self.users[key][4]))
-
-            print('Likes Received from each member and also what percent of the total likes received is from said member :')
-            for key_inner in self.users[key][5]:
-                percent = (self.users[key][5][key_inner] / self.users[key][2]) * 100
-                percent = round(percent, 2)
-                sys.stdout.write(self.users[key_inner][0])  # Name of liker
-                sys.stdout.write(' : ' + str(self.users[key][5][key_inner]) + ', ') # number of likes to user
-                sys.stdout.write(str(percent) + '%, ')
-            print
-
-            print('Percent of each member\'s total likes that went to ' + str(self.users[key][0]) + ': ')
-            for key_inner in self.users[key][5]:
-                sys.stdout.write(self.users[key_inner][0])
-                convert_to_percent = (self.users[key][5][key_inner] /
-                                      self.users[key_inner][7]) * 100
-                convert_to_percent = round(convert_to_percent, 2)
-                sys.stdout.write(': ' + str(convert_to_percent) + '%, ')
-            print
-
-            test = self.users[key][6]
-            print('Number of times you liked the same post as another member and what percent of the posts you '
-                  'liked were liked by that same member: ')
-            for key_inner in self.users[key][6]:
-                percent_shared = (self.users[key][6][key_inner]/self.users[key][7])*100
-                percent_shared = round(percent_shared, 2)
-                sys.stdout.write(self.users[key_inner][0])
-                sys.stdout.write(': ' + str(self.users[key][6][key_inner]) + ', ')
-                sys.stdout.write(str(percent_shared)+'%, ')
-            print
-            print
+            print(self.users[key]['name'])
+            print('Messages Sent: ' + str(self.users[key]["messages"]))
+            print('Likes Given: ' + str(self.users[key]["likes"]))
+            print('Likes Received: ' + str(self.users[key]["likes_received"]))
 
 if __name__ == "__main__":
     print(Analytics().response(""))
