5126b011f2e0f9ebe95c9dc1ed06e674018cbdc7
ErikBoesen <me@erikboesen.com>
2019-02-07 22:04:18 -0500

    Clean up 80% of data calculations

--- modules/analytics.py
+++ modules/analytics.py
@@ -31,10 +31,10 @@ class Analytics:
         self.populate_users(group['members'])
 
         # This line calls the "analyze_group" method which goes through the entire conversation
-        user_id_mapped_to_user_data = self.analyze_group(GROUP_ID, user_dictionary, message_count)
+        self.users = self.analyze_group(GROUP_ID, user_dictionary, message_count)
 
         #this line displays the data to the user
-        self.display_data(user_id_mapped_to_user_data)
+        self.display_data(self.users)
 
     def new_user(self, name):
         return {"name": name, "messages": 0, "likes": 0, "average_likes_per_message": 0.0, "average_likes_received": 0.0}
@@ -49,128 +49,100 @@ class Analytics:
             # [3] = average likes received per message, [4] = total words sent, [5] = dictionary of likes received from each member
             # [6] = dictionary of shared likes, [7] = total likes given
 
-    def analyze_group(self, GROUP_ID, user_id_mapped_to_user_data, message_count):
+    def analyze_group(self, GROUP_ID, self.users, message_count):
         message_id = 0
-        iterations = 0.0
+        message_number = 0
         while True:
-            response = requests.get('https://api.groupme.com/v3/groups/'+GROUP_ID+'/messages?token='+at)
+            response = requests.get('https://api.groupme.com/v3/groups/' + GROUP_ID + '/messages?token=' + at)
             messages = response.json()['response']['messages']
             for i in range(20):  # API sends 20 messages at once
-                try:
-                    iterations += 1
-                    name = messages[i]['name']
-                    message = messages[i]['text']
+                message_number += 1
+                name = messages[i]['name']
+                message = messages[i]['text']
 
-                    sender_id = messages[i]['sender_id']
-                    list_of_favs = messages[i]['favorited_by']
-                    length_of_favs = len(list_of_favs)
+                sender_id = messages[i]['sender_id']
+                likers = messages[i]['favorited_by']
+                likers_count = len(likers)
 
+                if sender_id not in self.users.keys():
+                    self.users[sender_id] = self.new_user(name)
 
-                    if sender_id not in user_id_mapped_to_user_data.keys():
-                        user_id_mapped_to_user_data[sender_id] = [name, 0.0, 0.0, 0.0, 0.0, {}, {}, 0.0]
+                # Fill the name in if user liked a message but hasn't yet been added to the dictionary
+                if self.users[sender_id]['name'] == '':
+                    self.users[sender_id]['name'] = name
 
-                    #this if statement is here to fill the name in for the case where a user id liked a message but had
-                    #yet been added to the dictionary
-                    if user_id_mapped_to_user_data[sender_id][0] == '':
-                        user_id_mapped_to_user_data[sender_id][0] = name
+                for liker_id in likers:
+                    if liker_id not in self.users.keys():
+                        # Leave name blank until user sends their first message
+                        self.users[user_id] = self.new_user('')
+                    self.users[liker_id]["likes"] += 1
 
-                    for user_id in list_of_favs:
-                        if user_id in user_id_mapped_to_user_data[sender_id][5].keys():
-                            user_id_mapped_to_user_data[sender_id][5][user_id] += 1
-                        else:
-                            user_id_mapped_to_user_data[sender_id][5][user_id] = 1
+                self.users[sender_id][1] += 1  # add one to sent message count
+                self.users[sender_id][2] += likers_count
 
-                    for user_id in list_of_favs:
-                        for user_id_inner in list_of_favs:
-                            if user_id not in user_id_mapped_to_user_data.keys():
-                                # leave name blank because this means a user is has liked a message but has yet to be added
-                                # to the dictionary. So leave the name blank until they send their first message.
-                                user_id_mapped_to_user_data[user_id] = ['', 0.0, 0.0, 0.0, 0.0, {}, {}, 0.0]
-                            if user_id == user_id_inner:
-                                user_id_mapped_to_user_data[user_id][7] += 1
-                                continue  # pass because you don't want to count yourself as sharing likes with yourself
-                            try:
-                                user_id_mapped_to_user_data[user_id][6][user_id_inner] += 1
-                            except KeyError:
-                                user_id_mapped_to_user_data[user_id][6][user_id_inner] = 1
-
-                    user_id_mapped_to_user_data[sender_id][1] += 1  # add one to sent message count
-                    user_id_mapped_to_user_data[sender_id][2] += length_of_favs
-
-                except IndexError:
-                    print("COMPLETE")
-                    print
-                    for key in user_id_mapped_to_user_data:
-                        try:
-                            user_id_mapped_to_user_data[key][3] = user_id_mapped_to_user_data[key][2] / user_id_mapped_to_user_data[key][1]
-                        except ZeroDivisionError:  # for the case where the user has sent 0 messages
-                            user_id_mapped_to_user_data[key][3] = 0
-                    return user_id_mapped_to_user_data
-
-            if i == 19:
-                    message_id = messages[i]['id']
-                    remaining = iterations/message_count
-                    remaining *= 100
-                    remaining = round(remaining, 2)
-                    print(str(remaining)+' percent done')
+            message_id = messages[19]['id']  # Get last message's ID for next request
+            remaining = message_number / message_count
+            remaining *= 100
+            remaining = round(remaining, 2)
+            print(str(remaining)+' percent done')
 
             payload = {'before_id': message_id}
             response = requests.get('https://api.groupme.com/v3/groups/'+GROUP_ID+'/messages?token='+at, params=payload)
             data = response.json()
 
-    def display_data(self, user_id_mapped_to_user_data):
-        for key in user_id_mapped_to_user_data:
-            print(user_id_mapped_to_user_data[key][0] + ' Data:')
-            print('Messages Sent: ' + str(user_id_mapped_to_user_data[key][1]))
-            print('Total Likes Given: ' + str(user_id_mapped_to_user_data[key][7]))
+    def display_data(self, self.users):
+        for key in self.users:
+            print(self.users[key][0] + ' Data:')
+            print('Messages Sent: ' + str(self.users[key][1]))
+            print('Total Likes Given: ' + str(self.users[key][7]))
             try:
-                print('Self Likes: ' + str(user_id_mapped_to_user_data[key][5][key]))
-                self_likes = user_id_mapped_to_user_data[key][5][key]
+                print('Self Likes: ' + str(self.users[key][5][key]))
+                self_likes = self.users[key][5][key]
             except KeyError:
                 self_likes = 0
                 print('Self Likes: ' + str(0))
-            print('Total Likes Received: ' + str(user_id_mapped_to_user_data[key][2]))
-            print('Average Likes Received Per Message: ' + str(user_id_mapped_to_user_data[key][3]))
+            print('Total Likes Received: ' + str(self.users[key][2]))
+            print('Average Likes Received Per Message: ' + str(self.users[key][3]))
 
             print('Total Likes Received with Self Likes Subtracted: ' +
-                  str(user_id_mapped_to_user_data[key][2] - self_likes))
+                  str(self.users[key][2] - self_likes))
 
-            total_likes_minus_self_likes = user_id_mapped_to_user_data[key][2] - self_likes
+            total_likes_minus_self_likes = self.users[key][2] - self_likes
             try:
-                new_avg = total_likes_minus_self_likes / user_id_mapped_to_user_data[key][1]
+                new_avg = total_likes_minus_self_likes / self.users[key][1]
             except ZeroDivisionError:  # for the case where the user has sent 0 messages
                 new_avg = 0
             print('Average Likes Received Per Message with Self Likes Subtracted: '
                   + str(new_avg))
 
-            print('Total Words Sent: ' + str(user_id_mapped_to_user_data[key][4]))
+            print('Total Words Sent: ' + str(self.users[key][4]))
 
             print('Likes Received from each member and also what percent of the total likes received is from said member :')
-            for key_inner in user_id_mapped_to_user_data[key][5]:
-                percent = (user_id_mapped_to_user_data[key][5][key_inner] / user_id_mapped_to_user_data[key][2]) * 100
+            for key_inner in self.users[key][5]:
+                percent = (self.users[key][5][key_inner] / self.users[key][2]) * 100
                 percent = round(percent, 2)
-                sys.stdout.write(user_id_mapped_to_user_data[key_inner][0])  # Name of liker
-                sys.stdout.write(' : ' + str(user_id_mapped_to_user_data[key][5][key_inner]) + ', ') # number of likes to user
+                sys.stdout.write(self.users[key_inner][0])  # Name of liker
+                sys.stdout.write(' : ' + str(self.users[key][5][key_inner]) + ', ') # number of likes to user
                 sys.stdout.write(str(percent) + '%, ')
             print
 
-            print('Percent of each member\'s total likes that went to ' + str(user_id_mapped_to_user_data[key][0]) + ': ')
-            for key_inner in user_id_mapped_to_user_data[key][5]:
-                sys.stdout.write(user_id_mapped_to_user_data[key_inner][0])
-                convert_to_percent = (user_id_mapped_to_user_data[key][5][key_inner] /
-                                      user_id_mapped_to_user_data[key_inner][7]) * 100
+            print('Percent of each member\'s total likes that went to ' + str(self.users[key][0]) + ': ')
+            for key_inner in self.users[key][5]:
+                sys.stdout.write(self.users[key_inner][0])
+                convert_to_percent = (self.users[key][5][key_inner] /
+                                      self.users[key_inner][7]) * 100
                 convert_to_percent = round(convert_to_percent, 2)
                 sys.stdout.write(': ' + str(convert_to_percent) + '%, ')
             print
 
-            test = user_id_mapped_to_user_data[key][6]
+            test = self.users[key][6]
             print('Number of times you liked the same post as another member and what percent of the posts you '
                   'liked were liked by that same member: ')
-            for key_inner in user_id_mapped_to_user_data[key][6]:
-                percent_shared = (user_id_mapped_to_user_data[key][6][key_inner]/user_id_mapped_to_user_data[key][7])*100
+            for key_inner in self.users[key][6]:
+                percent_shared = (self.users[key][6][key_inner]/self.users[key][7])*100
                 percent_shared = round(percent_shared, 2)
-                sys.stdout.write(user_id_mapped_to_user_data[key_inner][0])
-                sys.stdout.write(': ' + str(user_id_mapped_to_user_data[key][6][key_inner]) + ', ')
+                sys.stdout.write(self.users[key_inner][0])
+                sys.stdout.write(': ' + str(self.users[key][6][key_inner]) + ', ')
                 sys.stdout.write(str(percent_shared)+'%, ')
             print
             print
