fca6e2c4d0ff404abd580c00c691bfcdac68cbd2
ErikBoesen <me@erikboesen.com>
2019-02-08 20:59:32 -0500

    Fix members JSON parsing

--- purge.py
+++ purge.py
@@ -6,12 +6,12 @@ TOKEN = os.environ['GROUPME_ACCESS_TOKEN']
 
 def members(group_id):
     response = requests.get('https://api.groupme.com/v3/groups/%d?token=%s' % (group_id, TOKEN))
-    return response.json()['response']
+    return response.json()['response']['members']
 
 targets = ['Chike Iwuagwu', 'hiba hamid', 'AIDAN YOO(NOT SIMON)', 'Walli Chen (penn daddy)', 'Simon', 'Nick Bausenwein (Penn)', 'Eashan Sahai', 'AJ Manning', 'Margaret LaMarche', 'Rachel Wilson', 'Nitin S', 'Mia McDonald', 'David Yang', 'David Lu', 'serin b', 'Halsey Ziglar', 'Langston Luck', 'Sophie Hirt']
 target_ids = []
+
 for member in members(GROUP_ID):
     if member['nickname'] in targets:
         target_ids.append(member['id'])
 
-print(target_ids)
