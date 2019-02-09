ad3486881edc42b8eac0038b65cd92a7b052a2d1
ErikBoesen <me@erikboesen.com>
2019-02-08 21:04:57 -0500

    Actually kick members in purge

--- purge.py
+++ purge.py
@@ -4,14 +4,16 @@ import os
 GROUP_ID = 46649296
 TOKEN = os.environ['GROUPME_ACCESS_TOKEN']
 
-def members(group_id):
+def get_members(group_id):
     response = requests.get('https://api.groupme.com/v3/groups/%d?token=%s' % (group_id, TOKEN))
     return response.json()['response']['members']
 
-targets = ['Chike Iwuagwu', 'hiba hamid', 'AIDAN YOO(NOT SIMON)', 'Walli Chen (penn daddy)', 'Simon', 'Nick Bausenwein (Penn)', 'Eashan Sahai', 'AJ Manning', 'Margaret LaMarche', 'Rachel Wilson', 'Nitin S', 'Mia McDonald', 'David Yang', 'David Lu', 'serin b', 'Halsey Ziglar', 'Langston Luck', 'Sophie Hirt']
-target_ids = []
+def kick(group_id, target_id):
+    response = requests.get('https://api.groupme.com/v3/groups/%d/members/%s/remove?token=%s' % (group_id, TOKEN, target_id))
 
-for member in members(GROUP_ID):
+targets = ['Chike Iwuagwu', 'hiba hamid', 'AIDAN YOO(NOT SIMON)', 'Walli Chen (penn daddy)', 'Simon', 'Nick Bausenwein (Penn)', 'Eashan Sahai', 'AJ Manning', 'Margaret LaMarche', 'Rachel Wilson', 'Nitin S', 'Mia McDonald', 'David Yang', 'David Lu', 'serin b', 'Halsey Ziglar', 'Langston Luck', 'Sophie Hirt']
+
+for member in get_members(GROUP_ID):
     if member['nickname'] in targets:
-        target_ids.append(member['id'])
+        kick(GROUP_ID, member['id'])
 
