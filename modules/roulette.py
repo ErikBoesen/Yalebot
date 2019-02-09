e49e1a6164e8ac49132a94fe8f4ae1e9d791ae16
ErikBoesen <me@erikboesen.com>
2019-02-09 16:44:11 -0500

    Remove member who called !roulette

--- modules/roulette.py
+++ modules/roulette.py
@@ -4,14 +4,15 @@ from groupy.client import Client
 import os
 
 GROUP_ID = 46649296
-TOKEN = client = Client.from_token(os.environ['GROUPME_ACCESS_TOKEN'])
+client = Client.from_token(os.environ['GROUPME_ACCESS_TOKEN'])
 group = client.groups.get(id=GROUP_ID)
 
 class Roulette(Module):
     DESCRIPTION = "Choose a random person to kill"
     def response(self, query, message):
         if random.randint(1, 6) == 6:
-            victim = random.choice(group.members)
+            #victim = random.choice(group.members)
+            victim = group.member(id=message["sender_id"])
             victim.remove()
             return "Bang"
         else:
