6927cbce8509ef80b7dd1cd50102c024b12dc437
ErikBoesen <me@erikboesen.com>
2019-02-08 23:16:48 -0500

    Use Groupy for purge script

--- purge.py
+++ purge.py
@@ -1,20 +1,14 @@
-import requests
 import os
+from groupy.client import Client
 
 GROUP_ID = 46649296
 TOKEN = os.environ['GROUPME_ACCESS_TOKEN']
+client = Client.from_token(TOKEN)
 
-def get_members(group_id):
-    response = requests.get('https://api.groupme.com/v3/groups/%d?token=%s' % (group_id, TOKEN))
-    return response.json()['response']['members']
-
-def kick(group_id, target_id):
-    response = requests.post('https://api.groupme.com/v3/groups/%d/members/%s/remove?token=%s' % (group_id, TOKEN, target_id))
-    print(response.json())
+group = client.groups.get(id=GROUP_ID)
 
 targets = ['Chike Iwuagwu', 'hiba hamid', 'AIDAN YOO(NOT SIMON)', 'Walli Chen (penn daddy)', 'Simon', 'Nick Bausenwein (Penn)', 'Eashan Sahai', 'AJ Manning', 'Margaret LaMarche', 'Rachel Wilson', 'Nitin S', 'Mia McDonald', 'David Yang', 'David Lu', 'serin b', 'Halsey Ziglar', 'Langston Luck', 'Sophie Hirt']
 
-for member in get_members(GROUP_ID):
-    if member['nickname'] in targets:
-        kick(GROUP_ID, member['id'])
-
+for member in group.members:
+    if member.nickname in targets:
+        print(member.remove())
