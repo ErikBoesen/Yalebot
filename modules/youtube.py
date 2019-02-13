fe62373b78617e90e6b8e2c5ed724149d28d21cf
Victor Del Carpio <45403940+victor-hugo-dc@users.noreply.github.com>
2019-02-13 15:34:09 -0500

    Create youtube.py

---
+++ modules/youtube.py
@@ -0,0 +1,18 @@
+from .base import Module
+import requests
+from bs4 import BeautifulSoup
+
+class Youtube(Module):
+    DESCRIPTION = "Sends the first result on the YouTube search query"
+    url = 'https://www.youtube.com/results?search_query='
+    
+    def response(self, query, message):
+        url += query.replace(' ', '+').lower()
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        response = ''
+        for link in soup.find_all('a'):
+            result = link.get('href')
+            if result.startswith('/watch'):
+                response = link.get('href')
+                break
+        return 'https://www.youtube.com' + response
