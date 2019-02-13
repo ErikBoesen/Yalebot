9822aec8c7e86251dfe756e7b18f092b813bb4e6
Victor Del Carpio <45403940+victor-hugo-dc@users.noreply.github.com>
2019-02-13 09:16:27 -0500

    Add more groups

--- modules/groups.py
+++ modules/groups.py
@@ -5,6 +5,8 @@ class Groups(Module):
     links = {
         "University of Michigan": "46781389/hZehS1",
         "UNC Chapel Hill": "47739618/Ubl6Cn",
+        "University of Virginia": "47680748/aLDXS1",
+        "University of Miami": "48024399/XOwVQLZQ"
     }
     def response(self, query, message):
         return '\n'.join(["%s -> https://groupme.com/join_group/%s" % (name, self.links[name]) for name in self.links])
