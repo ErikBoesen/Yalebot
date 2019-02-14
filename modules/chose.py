fcfd93907dae975e403f9ba8d359ef5c99a78b55
ErikBoesen <me@erikboesen.com>
2019-02-13 20:08:51 -0500

    Fix thatswhyichoseyale spacing

--- modules/chose.py
+++ modules/chose.py
@@ -4,4 +4,4 @@ import random
 class Chose(Module):
     DESCRIPTION = "Sing our other favorite song"
     def response(self, query, message):
-        return "bum " * random.randint(0, 30) + ", that's why I chose Y" + "a" * random.randint(0, 30) + "le! https://www.youtube.com/watch?v=tGn3-RW8Ajk"
+        return "Bum" + " bum" * random.randint(0, 30) + ", that's why I chose Y" + "a" * random.randint(0, 30) + "le! https://www.youtube.com/watch?v=tGn3-RW8Ajk"
