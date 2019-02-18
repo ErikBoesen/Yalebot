f52b5f42ed139a6f74f7f6a783fd7d607582776b
ErikBoesen <me@erikboesen.com>
2019-02-17 19:24:43 -0500

    Welcome people on rejoin

--- modules/system.py
+++ modules/system.py
@@ -5,6 +5,6 @@ class System(Module):
     pass
 
 class Welcome(System):
-    RE = re.compile(r'(.+) added (.+) to the group\.|(.+) has joined the group')
+    RE = re.compile(r'(.+) added (.+) to the group\.|(.+) has (re)?joined the group')
     def response(self):
         return "Welcome! I'm Yalebot, a helpful tool for the Yale GroupMe. Type !help to see what I can do."
