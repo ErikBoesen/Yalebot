0d543d3487282b5883b73b780fea773c165be107
ErikBoesen <me@erikboesen.com>
2019-02-20 14:33:36 -0500

    Use environment variables for admits downloading script

--- admits.py
+++ admits.py
@@ -1,12 +1,15 @@
 import requests
 from requests.auth import HTTPDigestAuth
+import os
 
 ADMITS = 'https://apps.admissions.yale.edu/portal/admits?cmd=faces'
 LOGIN = 'https://apps.admissions.yale.edu/account/login'
-with requests.Session() as session:
-    payload = {
-        'email': 'erik.boesen@gmail.com',
-        'password': '',
-    }
-    r = session.post(LOGIN, data=payload)
-    print(r.text)
+session = requests.Session()
+
+payload = {
+    'email': os.environ['YALE_PORTAL_EMAIL'],
+    'password': os.environ['YALE_PORTAL_PASSWORD'],
+}
+r = session.post(LOGIN, data=payload)
+r = session.get(ADMITS + '&page=2')
+print(r.text)
