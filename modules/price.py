b9ff41450ee5c7efe00d88c44c8445ad9021349f
ErikBoesen <me@erikboesen.com>
2019-02-20 14:47:18 -0500

    Add description for Price

--- modules/price.py
+++ modules/price.py
@@ -4,6 +4,7 @@ SOCK_PRICE = 9.98
 SOCK_URL = "https://yale.bncollege.com/webapp/wcs/stores/servlet/TCK_Quarter_Sock/ProductDisplay?imageId=1359086&level=&graphicId=YAL2ROYAL-WHT-DKGREY&categoryId=40481&catalogId=10001&langId=-1&storeId=16556&productId=400000354407"
 
 class Price(Module):
+    DESCRIPTION = "Convert USD to YSK"
     def response(self, query, message):
         query = query.strip().strip('$')
         price = float(query)
