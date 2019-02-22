28bdca3d8518703c8c65bd96a7ef39286f05ac2b
ErikBoesen <me@erikboesen.com>
2019-02-21 21:42:34 -0500

    Add get_user method for welcoming

--- modules/system.py
+++ modules/system.py
@@ -8,3 +8,9 @@ class Welcome(System):
     RE = re.compile(r'(.+) added (.+) to the group\.|(.+) has (re)?joined the group')
     def response(self):
         return "Welcome! I'm Yalebot, a helpful tool for the Yale GroupMe. Type !help to see what I can do."
+    def get_user(self, query):
+        """
+        Get the name of the user described in the message.
+        :param query: message text to parse.
+        """
+        names = self.RE.findall(query)
