393b2827dbd69c9bc6dc982b499c44d0a111f248
ErikBoesen <me@erikboesen.com>
2019-02-23 09:38:38 -0500

    Use html.parser for BS

--- modules/lyrics.py
+++ modules/lyrics.py
@@ -8,10 +8,10 @@ class Lyrics(Module):
     def response(self, query, message):
         song_name = query
         r = requests.get(ENDPOINT + song_name.replace(' ', '+'))
-        bs = BeautifulSoup(r.text, 'lxml')
+        bs = BeautifulSoup(r.text, 'html.parser')
         link = bs.find('div', {'class': 'panel'}).find('a')
         r = requests.get(link['href'])
-        bs = BeautifulSoup(r.text, 'lxml')
+        bs = BeautifulSoup(r.text, 'html.parser')
         column = bs.find('div', {'class': 'col-xs-12 col-lg-8 text-center'})
         divs = column.find_all('div', recursive=False)
         text = column.find('b', recursive=False).text
