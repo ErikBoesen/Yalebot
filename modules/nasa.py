ccfd7c13a9be4977456d6dee7571135f660402be
ErikBoesen <me@erikboesen.com>
2019-02-23 15:08:57 -0500

    Fix error when no APOD key provided

--- modules/nasa.py
+++ modules/nasa.py
@@ -1,11 +1,12 @@
 from .base import Module
 import os
+import requests
 
 class NASA(Module):
     DESCRIPTION = 'See beautiful space photos'
     def response(self, query, message):
          # Get JSON data from NASA APOD API
-        photo = requests.get('https://api.nasa.gov/planetary/apod?api_key=%s' % (os.environ['APOD_KEY'] or 'DEMO_KEY')).json()
+        photo = requests.get('https://api.nasa.gov/planetary/apod?api_key=%s' % (os.environ.get('APOD_KEY') or 'DEMO_KEY')).json()
 
         # Send URL for image along with image's title
         reply(photo['url'])
