543c784b960ed1f17753fe2e3567fe7325988595
ErikBoesen <me@erikboesen.com>
2019-02-23 11:13:00 -0500

    Stop using custom headers for lyrics request

--- modules/lyrics.py
+++ modules/lyrics.py
@@ -6,18 +6,15 @@ ENDPOINT = 'https://www.lyricsfreak.com/search.php?q='
 
 class Lyrics(Module):
     DESCRIPTION = 'Because Googling song titles is too hard sometimes'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:67.0) Gecko/20100101 Firefox/67.0',
-    }
     def response(self, query, message):
         song_name = query
-        r = requests.get(ENDPOINT + song_name.replace(' ', '+'), headers=self.headers)
+        r = requests.get(ENDPOINT + song_name.replace(' ', '+'))
         bs = BeautifulSoup(r.text, 'html.parser')
         link = bs.find('a', {'class': 'song'})
         if link is None:
             return 'Could not find song "%s"' % song_name
         lyrics_page = 'https://www.lyricsfreak.com' + link['href']
-        r = requests.get(lyrics_page, headers=self.headers)
+        r = requests.get(lyrics_page)
         bs = BeautifulSoup(r.text, 'html.parser')
         column = bs.find('div', {'class': 'maincont d-cell al-t floatfix'})
         text = column.find('div', {'class': 'l_title'}).text.strip()
