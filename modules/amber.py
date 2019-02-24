13db40741cd76c5d33dd33584b0129a2ff2db40c
ErikBoesen <me@erikboesen.com>
2019-02-23 22:45:07 -0500

    Lowercase Amber things

--- modules/amber.py
+++ modules/amber.py
@@ -5,7 +5,7 @@ class Amber(Module):
     DESCRIPTION = 'aMbEr'
     def scramble(self, string):
         capitalize = bool(random.getrandbits(1))
-        return string.capitalize() if capitalize else string
+        return string.capitalize() if capitalize else string.lower()
 
     def response(self, query, message):
         return ''.join([self.scramble(char) for char in query])
