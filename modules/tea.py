17349641449b9f86120e8ae06a08aaded33eb954
Victor Del Carpio <vhdelcg@gmail.com>
2019-03-03 13:23:08 -0500

    Create tea.py

---
+++ modules/tea.py
@@ -0,0 +1,25 @@
+from .base import Module
+
+class Tea(Module):
+    DESCRIPTION = 'Spills the tea.'
+    def response(self, query, message):
+        msg1 = '{0: <13}'.format(query[0:13])
+        msg2 = '{0: <11}'.format(query[13:24])
+        msg3 = '{0: <9}'.format(query[24:33])
+        msg4 = '{0: <7}'.format(query[33:40])
+    
+        tea = '''
+        today’s tea:
+                
+                  (
+              )      )
+            )     (      )   
+        _(___(____)____(___(_
+        \                   /__
+         \  {str1}  /   |
+          \  {str2}  /____|
+           \  {str3}  /
+            \  {str4}  /
+             \_________/
+             '''.format(str1=msg1, str2 = msg2, str3 = msg3, str4 = msg4)
+        return tea
