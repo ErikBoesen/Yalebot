7f2cb9b81a253cd5dd7a278821854646bed45545
ErikBoesen <me@erikboesen.com>
2019-03-06 16:12:56 -0500

    Fix loading image data from URL

--- modules/uwu.py
+++ modules/uwu.py
@@ -3,33 +3,32 @@ import face_recognition
 from PIL import Image, ImageDraw
 import requests
 import numpy as np
+from io import BytesIO
 
 class UWU(Module):
     DESCRIPTION = 'Abuse photographs of your compatriots'
-    uwu = Image.open('uwu.png')
-    draw = ImageDraw.Draw(pil_image)
+    uwu = Image.open('resources/uwu.png')
     def response(self, query, message):
         # Get sent image
-        source_url = [attachment for attachment in message['attachments'] if attachment['type'] == 'image'][0].get('image')
+        source_url = [attachment for attachment in message['attachments'] if attachment['type'] == 'image'][0].get('url')
         # If no image was sent, use sender's avatar
-        source_url = source_url or attachment['avatar_url']
+        source_url = source_url or message['avatar_url']
 
         r = requests.get(source_url)
-        raw_image = r.text
-        pil_image = Image.frombytes(raw_image)
-        faces = face_recognition.face_locations(image)
+        pil_image = Image.open(BytesIO(r.content))
+        faces = face_recognition.face_locations(np.array(pil_image.getdata()))
+        print(faces)
         for face in faces:
             top, right, bottom, left = face
 
             # Scale uwu mask
-            width, height = uwu.size
+            width, height = self.uwu.size
             uwu_width = int((right - left) * 0.6)
             uwu_height = int(uwu_width * height / width)
-            uwu = uwu.resize((uwu_width, uwu_height), Image.ANTIALIAS)
+            scaled_uwu = self.uwu.resize((uwu_width, uwu_height), Image.ANTIALIAS)
 
-            pil_image.paste(uwu, (left + int(0.2 *1.0/0.6* uwu_width), top + (bottom - top) // 5), uwu)
+            pil_image.paste(scaled_uwu, (left + int(0.2 *1.0/0.6* uwu_width), top + (bottom - top) // 5), scaled_uwu)
 
         pil_image.save('out.png')
 
-if __name__ == '__main__':
-    print(UWU.response('', {'attachments': [{'type': 'image', 'url': 'https://i.groupme.com/1296x972.jpeg.91ab436782064b278a647dd7cf924c81.preview'}]}))
+print(UWU().response('', {'attachments': [{'type': 'image', 'url': 'https://i.groupme.com/1296x972.jpeg.91ab436782064b278a647dd7cf924c81.preview'}]}))
