c6d7f03e0781e31186e93a031c80f18e0f72fd1f
ErikBoesen <me@erikboesen.com>
2019-03-06 14:45:14 -0500

    Successfully do uwu processing in test script

--- beta/uwu.py
+++ beta/uwu.py
@@ -3,10 +3,18 @@ from PIL import Image, ImageDraw
 
 image = face_recognition.load_image_file('jt.png')
 pil_image = Image.fromarray(image)
+uwu = Image.open('uwu.png')
 draw = ImageDraw.Draw(pil_image)
 faces = face_recognition.face_locations(image)
-landmarks = face_recognition.face_landmarks(image)
-for landmark in landmarks:
-    draw.polygon(landmark['left_eye'], fill=(255, 255, 255, 20))
+for face in faces:
+    top, right, bottom, left = face
+
+    # Scale uwu mask
+    width, height = uwu.size
+    uwu_width = int((right - left) * 0.6)
+    uwu_height = int(uwu_width * height / width)
+    uwu = uwu.resize((uwu_width, uwu_height), Image.ANTIALIAS)
+
+    pil_image.paste(uwu, (left + int(0.2 *1.0/0.6* uwu_width), top + (bottom - top) // 5), uwu)
 
 pil_image.save('out.png')
