57149f2d1195c0fa7ffc36472b7f85cb0675adca
ErikBoesen <me@erikboesen.com>
2019-03-07 09:17:44 -0500

    Add more space in teacup

--- modules/tea.py
+++ modules/tea.py
@@ -6,17 +6,20 @@ CUP = '''>Today's tea:
       )      )
     )     (      )
 _(___(____)____(___(_
-\  {str0}  /__
- \  {str1}  /   |
-  \  {str2}  /____|
-   \  {str3}  /
-    \  {str4}  /
+\ {str0} /__
+ \ {str1} /   |
+  \ {str2} /____|
+   \ {str3} /
+    \ {str4} /
      \_________/'''
 
 class Tea(Module):
     DESCRIPTION = 'Spills the tea.'
     def width(self, row):
-        return 15 - 2 * row
+        """
+        Given the number of the row, calculate how long that row should be.
+        """
+        return 17 - 2 * row
     def response(self, query, message):
         lines = []
         offset = 0
@@ -26,5 +29,3 @@ class Tea(Module):
 
         tea = CUP.format(str0=lines[0], str1=lines[1], str2=lines[2], str3=lines[3], str4=lines[4]).replace(' ', ' ')
         return tea
-
-print(Tea().response('this is a test', None))
