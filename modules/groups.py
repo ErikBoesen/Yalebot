842d44daf3ca32e2fab1de8b4dbbf03d15f0118f
ErikBoesen <me@erikboesen.com>
2019-03-06 22:05:17 -0500

    Add new UNC link

--- modules/groups.py
+++ modules/groups.py
@@ -4,7 +4,7 @@ class Groups(Module):
     DESCRIPTION = "Get links to other admit groups"
     links = {
         "University of Michigan": "46781389/hZehS1",
-        "UNC Chapel Hill": "47739618/Ubl6Cn",
+        "UNC Chapel Hill": "47712261/2K9zx7",
         "University of Virginia": "47680748/aLDXS1",
         "University of Miami": "48024399/XOwVQLZQ",
         "Baylor University": "42341652/aTexzNBA",
