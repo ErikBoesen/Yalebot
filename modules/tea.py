15715852972f7d0ea25d30ab839e4d0a9d6be485
ErikBoesen <me@erikboesen.com>
2019-03-07 08:58:51 -0500

    Use top row for tea

--- modules/tea.py
+++ modules/tea.py
@@ -6,7 +6,7 @@ CUP = '''>Today's tea:
       )      )
     )     (      )
 _(___(____)____(___(_
-\                   /__
+\  {str0}  /__
  \  {str1}  /   |
   \  {str2}  /____|
    \  {str3}  /
@@ -16,9 +16,10 @@ _(___(____)____(___(_
 class Tea(Module):
     DESCRIPTION = 'Spills the tea.'
     def response(self, query, message):
-        msg1 = '{0: <13}'.format(query[0:13])
-        msg2 = '{0: <11}'.format(query[13:24])
-        msg3 = '{0: <9}'.format(query[24:33])
+        msg0 = '{0: <15}'.format(query[0:15])
+        msg1 = '{0: <13}'.format(query[15:28])
+        msg2 = '{0: <11}'.format(query[28:39])
+        msg3 = '{0: <9}'.format(query[39:33])
         msg4 = '{0: <7}'.format(query[33:40])
 
         tea = CUP.format(str1=msg1, str2 = msg2, str3 = msg3, str4 = msg4).replace(' ', ' ')
