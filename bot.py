42937131d75054c89b35ba18d3cb728ba7bb5fc8
ErikBoesen <me@erikboesen.com>
2019-03-07 19:06:31 -0500

    Fix zero parameters registering as 1

--- bot.py
+++ bot.py
@@ -89,14 +89,17 @@ def webhook():
                 reply(simple_responses[command], group_id)
             elif command in commands:
                 # If not, query appropriate module for a response
-                if len(query.split("\n")) < commands[command].ARGC:
+                if len(list(filter(None, query.split("\n")))) < commands[command].ARGC:
                     reply("Not enough parameters!", group_id)
                 else:
                     response = commands[command].response(query, message)
                     if response is not None:
                         reply(response, group_id)
             elif command in meme_commands:
-                if len(query.split("\n")) < meme_commands[command].ARGC:
+                # TODO: This is clumsy logic. First of all it's done twice, first in the normal commands
+                # section, and then again here. Second, meme commands split the query into lines
+                # already; we're just doing it twice with this.
+                if len(list(filter(None, query.split("\n")))) < meme_commands[command].ARGC:
                     reply("Not enough captions (make sure to separate with newlines).", group_id)
                 else:
                     #reply("@" + message["name"], group_id, image=meme_commands[command].response(query))
