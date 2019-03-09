16c427490774901f291d21105ab87bc187cb37c5
ErikBoesen <me@erikboesen.com>
2019-03-09 18:14:20 -0500

    Implement FunFact module

--- modules/funfact.py
+++ modules/funfact.py
@@ -1,6 +1,15 @@
 from .base import Module
+import requests
 
-class FunFact(Module)
+class FunFact(Module):
     DESCRIPTION = "Learn a fun fact"
+    facts = []
+
+    def fill_queue(self):
+        raw = requests.get("http://mentalfloss.com/api/facts?page=4&limit=1").json()
+        self.facts = [entry["fact"] for entry in raw]
+
     def response(self, query, message):
-        return "Unimplemented"
+        if len(self.facts) == 0:
+            self.fill_queue()
+        return self.facts.pop()
