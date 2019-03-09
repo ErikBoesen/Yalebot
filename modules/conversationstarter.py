36ce9eddfad512d1d2bdd0cbca1fb1e791bf1565
ErikBoesen <me@erikboesen.com>
2019-03-09 15:23:47 -0500

    Add DESCRIPTION for conversationstarter

--- modules/conversationstarter.py
+++ modules/conversationstarter.py
@@ -2,6 +2,7 @@ from .base import Module
 import requests
 
 class ConversationStarter(Module):
+    DESCRIPTION = "Generate an icebreaker question"
     def response(self, query, message):
         question = requests.post("https://www.conversationstarters.com/random.php").text
         # Trim off <img> tag from start
