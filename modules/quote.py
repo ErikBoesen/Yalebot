a8c1b8fb0eb28ecec2ad3eaaac04262d7f04355d
ErikBoesen <me@erikboesen.com>
2019-03-09 15:55:21 -0500

    Add inspirobot function

---
+++ modules/quote.py
@@ -0,0 +1,7 @@
+from .base import Module
+import requests
+
+class Quote(Module):
+    DESCRIPTION = "Generate inspirational quotes"
+    def response(self, query, message):
+        return requests.get("https://inspirobot.me/api?generate=true").text
