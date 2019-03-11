553be7014ef8d1c1adca7cc066e64a32efac5275
ErikBoesen <me@erikboesen.com>
2019-03-11 18:42:11 -0400

    Add create_bot.py

---
+++ create_bot.py
@@ -0,0 +1,17 @@
+import requests
+import os
+import json
+from pick import pick
+
+with open("groups.json", "r") as f:
+    groups = json.load(f)
+group_name = pick([groups[group_id]["name"] for group_id in groups])[0]
+# Knowing name chosen, get group ID
+for candidate in groups:
+    if groups[candidate]["name"] == group_name:
+        group_id = candidate
+        break
+print(f"Selected group {group_id}/{group_name}.")
+
+text = input("Message: ")
+requests.post("https://api.groupme.com/v3/bots/post", data={"text": text, "bot_id": groups[group_id]["bot_id"]})
