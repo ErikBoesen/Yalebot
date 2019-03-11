d4eaf485f31322b146876abb6271ae3fb4d49fc0
ErikBoesen <me@erikboesen.com>
2019-03-11 18:35:36 -0400

    Check for correct group_name right

--- send.py
+++ send.py
@@ -8,9 +8,9 @@ with open("groups.json", "r") as f:
 group_name = pick([groups[group_id]["name"] for group_id in groups])
 # Knowing name chosen, get group ID
 for candidate in groups:
-    if groups[candidate]["name"]:
+    if groups[candidate]["name"] == group_name:
         group_id = candidate
 print(group_id)
 
-text = input('Message: ')
+text = input("Message: ")
 requests.post("https://api.groupme.com/v3/bots/post", data={"text": text, "bot_id": groups[group_id]["bot_id"]})
