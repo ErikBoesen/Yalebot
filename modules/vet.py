e478ce1c1ed314a023b4e1564a48ed6b6cac6780
ErikBoesen <me@erikboesen.com>
2019-03-11 17:07:45 -0400

    Vet all users if no user given

--- modules/vet.py
+++ modules/vet.py
@@ -4,7 +4,7 @@ import json
 
 class Vet(Module):
     DESCRIPTION = "Check if users are actually Yale admits"
-    ARGC = 1
+    ARGC = 0
     POSITIVE_EMOJI = "😀😁😄😍👌\U0001f91f👏⭐️🔥\U0001f9e1💛💚💙💜🖤✅"
     NEGATIVE_EMOJI = '\U0001f928😨😰😱🤔👎🌧❌🚫'
     def __init__(self):
@@ -12,7 +12,18 @@ class Vet(Module):
         with open('resources/admit_names.json') as f:
             self.admits = json.load(f)
 
+    def get_members(self, group_id):
+        members = requests.get("https://api.groupme.com/v3/groups/%d/members?token=%s" % (group_id, token)).json()["response"]["members"]
+        return [member["name"] for member in members]
+
     def response(self, query, message):
+        if not query:
+            # TODO: Messy
+            string = ""
+            members = self.get_members(message["group_id"])
+            for member in members:
+                string += self.check_user(member)
+            return string
         return self.check_user(query.strip('@'))
 
     def is_admit(self, name: str):
