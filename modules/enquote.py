a594764f181fd3fa4b31bebe7d3249fb9b56f255
Omar Elhosseni <32843273+OmarElhosseni@users.noreply.github.com>
2019-03-13 11:51:06 -0400

    Update enquote.py

--- modules/enquote.py
+++ modules/enquote.py
@@ -4,7 +4,7 @@ class ENQUOTE(Module):
     DESCRIPTION = "writes quote en quote however many times"
     ARGC = 1
     def response(self, query, message):
-	if(query < 0 || query > 10):
+	if(query < 0 or query > 10):
 		query = 1
         return "quote en" + " quote en"*int(query)
 
