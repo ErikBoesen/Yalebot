dc1762085ea4b1dd6d97cfbca356f318b7763cd6
ErikBoesen <me@erikboesen.com>
2019-03-13 15:52:39 -0400

    Fix capitalization in bot creation

--- manage.py
+++ manage.py
@@ -21,6 +21,9 @@ def get_user_groups():
 def get_joined_groups():
     with open(args.groups_file, "r") as f:
         return json.load(f)
+def save_groups(groups):
+    with open(args.groups_file, "w") as f:
+        json.dump(groups, f)
 
 def pick_joined_group(groups=None) -> str:
     """
@@ -40,10 +43,8 @@ def pick_user_group(groups=None) -> str:
     """
     :return:
     """
-    print(groups)
     if groups is None:
         groups = get_user_groups()
-    print(groups)
     group_name = pick([group["name"] for group in groups])[0]
     # Knowing name chosen, get group id
     for candidate in groups:
@@ -54,33 +55,31 @@ def pick_user_group(groups=None) -> str:
 
 
 if args.verb == "create_bot":
-    groups = get_joined_groups()
-    group_id = pick_user_group(groups)
+    group_id = pick_user_group()
 
     bot = {
-        "name": read("name", "yalebot"),
+        "name": read("Name", "Yalebot"),
         "group_id": group_id,
-        "avatar_url": read("avatar url", "https://i.groupme.com/310x310.jpeg.1c88aac983ff4587b15ef69c2649a09c"),
-        "callback_url": read("callback url", "https://yalebot.herokuapp.com/"),
-        "dm_notification": false,
+        "avatar_url": read("Avatar URL", "https://i.groupme.com/310x310.jpeg.1c88aac983ff4587b15ef69c2649a09c"),
+        "callback_url": read("Callback URL", "https://yalebot.herokuapp.com/"),
+        "dm_notification": False,
     }
     result = requests.post(f"https://api.groupme.com/v3/bots?token={args.token}",
                            json={"bot": bot}).json()["response"]["bot"]
 
+    groups = get_joined_groups()
     groups[result["group_id"]] = {
         "name": bot["name"],
         "bot_id": result["bot_id"],
     }
-    with open(args.groups_file, "r+") as f:
-        json.dump(groups, f)
+    store_groups(groups)
 elif args.verb == "destroy_bot":
     group_id = pick_joined_group()
     request = requests.post(f"https://api.groupme.com/v3/bots/destroy?token={args.token}", data={"bot_id": groups[group_id]["bot_id"]})
     if request.ok:
         print("Success.")
-        with open(args.groups_file, "w") as f:
-            del groups[group_id]
-            json.dump(groups, f)
+        del groups[group_id]
+        store_groups(groups)
     else:
         print("Failure: ", end="")
         print(request.json())
