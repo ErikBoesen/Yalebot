7e062b69b426ca4c7d7cb57247ad9a297ade420c
Omar Elhosseni <32843273+OmarElhosseni@users.noreply.github.com>
2019-03-13 11:26:40 -0400

    Delete enquote.py

--- enquote.py
+++
@@ -1,8 +0,0 @@
-from .base import Module
-	
-class ENQUOTE(Module):
-    DESCRIPTION = "writes quote en quote however many times"
-    ARGC = 1
-    def response(self, query, message):
-        return "quote en" + " quote en"*int(query)
-
