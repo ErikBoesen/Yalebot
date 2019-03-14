061b3988d6dc7f3ef3aab6b1caf81937c5ca89b1
Victor Del Carpio <vhdelcg@gmail.com>
2019-03-14 08:28:39 -0400

    Use f strings

--- modules/twitter.py
+++ modules/twitter.py
@@ -11,16 +11,16 @@ class Twitter(Module):
             qlist = query.split(',', 1)
             query = qlist[0].strip()
             msg = qlist[1].strip()
-        url = 'http://gettwitterid.com/?user_name={user}&submit=GET+USER+ID'.format(user=query)
+        url = f'http://gettwitterid.com/?user_name={query}&submit=GET+USER+ID'
         soup = BeautifulSoup(requests.get(url).text, 'html.parser')
         try:
             id = str(soup.find_all('p')[2])
             id = id[3:id.__len__()-4]
         except IndexError:
-            return 'Twitter handle must be incorrect or sumn'
+            return 'Twitter handle does not exist'
 
         if msg is not '':
             msg = msg.replace(' ', '%20')
-            return 'https://twitter.com/messages/compose?recipient_id='+id+'&text='+msg
+            return f'https://twitter.com/messages/compose?recipient_id={id}&text={msg}'
         else:
             return 'https://twitter.com/messages/compose?recipient_id='+id
