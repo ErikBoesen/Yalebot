b96b9494a2227bcc2257f958ab0a6efbebebed5a
ErikBoesen <me@erikboesen.com>
2019-03-15 17:00:57 -0400

    Remove extraneous function in about module

--- modules/about.py
+++ modules/about.py
@@ -3,7 +3,7 @@ from glob import glob
 import os
 
 class About(Module):
-    def counts(self):
+    def response(self, query, message):
         total_lines = 0
         most_lines = 0
         longest_file = None
@@ -14,8 +14,5 @@ class About(Module):
                 if lines > most_lines:
                     most_lines = lines
                     longest_file = filename
-        return (total_lines, longest_file, most_lines)
 
-    def response(self, query, message):
-        total_lines, longest_file, most_lines = self.counts()
         return f"I am a bot maintained by Erik Bøsen, whom you should follow on Instagram @erikboesen. Use the command !help to view a list of bot capabilities. The bot's source code can be viewed and contributed to on GitHub: https://github.com/ErikBoesen/Yalebot\n\nI am currently running on {total_lines} lines of code. My largest file is {longest_file}, at {most_lines} lines.",
