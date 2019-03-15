9dbac078d6ae29d2c4f294121d5aea1ddaaf4898
ErikBoesen <me@erikboesen.com>
2019-03-15 19:50:34 -0400

    Fix not formatting XKCD string

--- modules/xkcd.py
+++ modules/xkcd.py
@@ -4,6 +4,6 @@ import requests
 class XKCD(Module):
     DESCRIPTION = "View the most recent XKCD comic, or specify a comic by number"
     def response(self, query, message):
-        comic = requests.get("http://xkcd.com/{query}/info.0.json" if query else "http://xkcd.com/info.0.json").json()
-        return ["\"" + comic['alt'] + "\"",
+        comic = requests.get(f"http://xkcd.com/{query}/info.0.json" if query else "http://xkcd.com/info.0.json").json()
+        return ["\"" + comic["alt"] + "\"",
                 comic["img"]]
