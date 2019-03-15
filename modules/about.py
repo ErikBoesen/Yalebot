d5f414eb69e1476c9ba91e07a81da548a5e20d77
ErikBoesen <me@erikboesen.com>
2019-03-15 16:49:26 -0400

    Fix attempting to append list to int in about

--- modules/about.py
+++ modules/about.py
@@ -9,7 +9,7 @@ class About(Module):
         longest_file = None
         for filename in glob("*.py") + glob("modules/*.py"):
             with open(filename) as f:
-                lines = f.readlines()
+                lines = len(f.readlines())
                 total_lines += lines
                 if lines > most_lines:
                     most_lines = lines
@@ -19,5 +19,3 @@ class About(Module):
     def response(self, query, message):
         total_lines, longest_file, most_lines = self.counts()
         return f"I am a bot maintained by Erik Bøsen, whom you should follow on Instagram @erikboesen. Use the command !help to view a list of bot capabilities. The bot's source code can be viewed and contributed to on GitHub: https://github.com/ErikBoesen/Yalebot\n\nI am currently running on {total_lines} lines of code. My largest file is {longest_file}, at {most_lines} lines.",
-
-print(About().response("", {}))
