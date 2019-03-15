3706ff283bd75bb6112124b5b2d1262887d9f1b0
ErikBoesen <me@erikboesen.com>
2019-03-15 19:39:06 -0400

    Respond with multiple messages in xkcd

--- modules/xkcd.py
+++ modules/xkcd.py
@@ -4,4 +4,5 @@ import requests
 class XKCD(Module):
     def response(self, query, message):
         comic = requests.get("http://xkcd.com/{query}/info.0.json" if query else "http://xkcd.com/info.0.json").json()
-        return "Hover text: \"" + comic['alt'] + "\n" + comic["img"]
+        return ["\"" + comic['alt'] + "\"",
+                comic["img"]]
