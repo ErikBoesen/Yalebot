d77b2a710226e6e612d2176e6b2572d16c7672fc
ErikBoesen <me@erikboesen.com>
2019-03-17 12:58:10 -0400

    Only get dogs list when command is first run

--- modules/dog.py
+++ modules/dog.py
@@ -9,15 +9,15 @@ class Dog(Module):
     DESCRIPTION = "Get learned about a pupper"
     dogs = []
     CITATION = re.compile(r'\[\d+\]')
-    def __init__(self):
-        super().__init__()
-        r = requests.get("https://en.wikipedia.org/wiki/List_of_individual_dogs")
-        bs = BeautifulSoup(r.text, "html.parser")
-        lists = bs.find("div", {"class": "mw-parser-output"}).find_all("ul", recursive=False)
-        for ul in lists:
-            self.dogs += [BeautifulSoup(str(li), "html.parser").text for li in ul]
-        # Filter out empty items
-        self.dogs = [self.CITATION.sub('', dog) for dog in self.dogs if len(dog) > 5]
-
     def response(self, query, message):
+        if len(self.dogs) == 0:
+            r = requests.get("https://en.wikipedia.org/wiki/List_of_individual_dogs")
+            bs = BeautifulSoup(r.text, "html.parser")
+            lists = bs.find("div", {"class": "mw-parser-output"}).find_all("ul", recursive=False)
+            dogs = []
+            for ul in lists:
+                dogs += [BeautifulSoup(str(li), "html.parser").text for li in ul]
+            # Filter out empty items and remove citations
+            dogs = [self.CITATION.sub('', dog) for dog in self.dogs if len(dog) > 5]
+            self.dogs = dogs
         return random.choice(self.dogs)
