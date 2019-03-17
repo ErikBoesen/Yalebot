fa2fb94e31ffd47cb033b838701dd5ce058dc76e
ErikBoesen <me@erikboesen.com>
2019-03-17 17:39:43 -0400

    Write code to find items in IMDb list

--- modules/jake.py
+++ modules/jake.py
@@ -1,11 +1,20 @@
 from .base import Module
-from imdb import IMDb
+from bs4 import BeautifulSoup
+import requests
+import random
 
 class Jake(Module):
+    musicals = []
     def __init__(self):
         super().__init__()
-        self.imdb = IMDb()
 
     def response(self, query, message):
         if len(self.musicals) == 0:
-            print(self.imdb.
+            bs = BeautifulSoup(requests.get("https://www.imdb.com/list/ls000071646/").text, 'html.parser')
+            musical_elements = bs.find("div", {"class": "lister-list"}).find_all("div", {"class": "lister-item"})
+            for entry in musical_elements:
+                musicals.append((entry.find("h3", {"class": "lister-item-header"}).find("a", recursive=False).text,
+                                 " | ".join([p.text for p in entry.find_all("p")]))
+        return random.choice(self.musicals)
+
+print(Jake().response("",""))
