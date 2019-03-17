2874ea54528a4105c784c9ab1fd8fd54c93a9209
ErikBoesen <me@erikboesen.com>
2019-03-17 12:50:53 -0400

    Fix citations in dog

--- modules/dog.py
+++ modules/dog.py
@@ -2,11 +2,13 @@ from .base import Module
 import requests
 from bs4 import BeautifulSoup
 import random
+import re
 
 
 class Dog(Module):
-    DESCRIPTION = "Learn about a pupper"
+    DESCRIPTION = "Get learned about a pupper"
     dogs = []
+    CITATION = re.compile(r'\[\d+\]')
     def __init__(self):
         super().__init__()
         r = requests.get("https://en.wikipedia.org/wiki/List_of_individual_dogs")
@@ -14,7 +16,8 @@ class Dog(Module):
         lists = bs.find("div", {"class": "mw-parser-output"}).find_all("ul", recursive=False)
         for ul in lists:
             self.dogs += [BeautifulSoup(str(li), "html.parser").text for li in ul]
-        self.dogs = [dog for dog in self.dogs if len(dog) > 5]
+        # Filter out empty items
+        self.dogs = [self.CITATION.sub('', dog) for dog in self.dogs if len(dog) > 5]
 
     def response(self, query, message):
         return random.choice(self.dogs)
