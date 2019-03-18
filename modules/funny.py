9cfeb3c132215aa059aea279645fe00a1eda0881
ErikBoesen <me@erikboesen.com>
2019-03-18 09:01:27 -0400

    Use queue for !funny

--- modules/funny.py
+++ modules/funny.py
@@ -5,12 +5,16 @@ import random
 
 class Funny(Module):
     DESCRIPTION = "Get random meme from r/Memes"
+    responses = []
+    def __init__(self):
+        super().__init__()
+        self.reddit = praw.Reddit(client_id=os.environ["REDDIT_CLIENT_ID"],
+                                  client_secret=os.environ["REDDIT_SECRET"],
+                                  user_agent="yalebot")
+
     def response(self, query, message):
-        reddit = praw.Reddit(client_id=os.environ["REDDIT_CLIENT_ID"],
-                             client_secret=os.environ["REDDIT_SECRET"],
-                             user_agent="yalebot")
-        response = []
-        for submission in reddit.subreddit("memes").hot(limit=25):
-            if not submission.stickied and "jpg" in submission.url:
-                response.append(submission.url)
-        return random.choice(response)
+        if len(self.responses) == 0:
+            for submission in reddit.subreddit("memes").hot(limit=25):
+                if not submission.stickied and "jpg" in submission.url:
+                    self.responses.append(submission.url)
+        return random.choice(self.responses)
