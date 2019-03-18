9e838f98b32a28fbadf1f76b636fa00f2a66524d
ErikBoesen <me@erikboesen.com>
2019-03-18 08:58:02 -0400

    Increase limit for reddit fetching

--- modules/funny.py
+++ modules/funny.py
@@ -10,7 +10,7 @@ class Funny(Module):
                              client_secret=os.environ["REDDIT_SECRET"],
                              user_agent='yalebot')
         response = []
-        for submission in reddit.subreddit('memes').hot(limit=15):
+        for submission in reddit.subreddit('memes').hot(limit=25):
             if not submission.stickied and 'jpg' in submission.url:
                 response.append(submission.url)
         return random.choice(response)
