c95ba40db859bd9ab809c8d5c84a4525c7c4be7c
ErikBoesen <me@erikboesen.com>
2019-03-25 15:33:01 -0400

    Fix maria removing the wrong letters

--- modules/maria.py
+++ modules/maria.py
@@ -5,4 +5,4 @@ class Maria(Module):
     DESCRIPTION = "Mdl fr Mr's hmwrk"
 
     def response(self, query, message):
-        return "".join([character for character in query if character.lower() not in ("a", "b", "c", "d", "e")])
+        return "".join([character for character in query if character.lower() not in "aeiouy"])
