1c9d05ba664b8beb9acc5681cb8f4a20e4b70cac
ErikBoesen <me@erikboesen.com>
2019-03-29 09:44:26 -0400

    Only welcome people with their first names

--- modules/system.py
+++ modules/system.py
@@ -10,7 +10,8 @@ class Welcome(System):
     RE = re.compile(r'(.+) added (.+) to the group\.|(.+) has (re)?joined the group')
 
     def response(self, query, message):
-        return "👋 Welcome " + ", ".join(self.get_names(query)) + "! We're happy to have you. I'm Yalebot, a bot for the Yale 2023 GroupMe. Type !help to see what I can do."
+        names = ", ".join([name.split(" ", 1)[0] for name in self.get_names(query)])
+        return "👋 Welcome " + names + "! We're happy to have you. I'm Yalebot, a bot for the Yale 2023 GroupMe. Type !help to see what I can do."
 
     def get_names(self, query: str) -> str:
         """
