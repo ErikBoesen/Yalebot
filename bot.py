d35c7540c302e1ab1f7083bc4bbebf7b84719f1c
ErikBoesen <me@erikboesen.com>
2019-03-31 14:56:39 -0400

    Exclude used groups when a database is linked

--- bot.py
+++ bot.py
@@ -214,7 +214,7 @@ def manager():
     access_token = request.args["access_token"]
     groups = requests.get(f"https://api.groupme.com/v3/groups?token={access_token}").json()["response"]
     bots = requests.get(f"https://api.groupme.com/v3/bots?token={access_token}").json()["response"]
-    if False:
+    if os.environ.get("DATABASE_URL") is not None:
         groups = [group for group in groups if not Bot.query.get(group["group_id"])]
         bots = [bot for bot in bots if Bot.query.get(bot["group_id"])]
     return render_template("manager.html", access_token=access_token, groups=groups, bots=bots)
