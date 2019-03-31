07799c7ef7354180ca442a3c5d9666b0010f8893
ErikBoesen <me@erikboesen.com>
2019-03-31 17:22:05 -0400

    Delete privacy policy and terms of service

--- static/scripts/manager.js
+++ static/scripts/manager.js
@@ -1,7 +1,3 @@
-document.getElementById('accept').onclick = function() {
-    document.getElementById('add').disabled = !this.checked;
-}
-
 onclick = function(e) {
     if (e.target.tagName == 'BUTTON' && e.target.classList.contains('delete')) {
         if (confirm('Really delete bot?')) {
