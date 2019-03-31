934113bf0584bd01cdfb4e2ce0564a7bb9052034
ErikBoesen <me@erikboesen.com>
2019-03-31 14:11:23 -0400

    Fix getting access token for deletion

--- static/scripts/controlpanel.js
+++ static/scripts/controlpanel.js
@@ -6,7 +6,13 @@ onclick = function(e) {
     if (e.target.tagName == 'BUTTON' && e.target.classList.contains('delete')) {
         if (confirm('Really delete bot?')) {
             var req = new XMLHttpRequest();
-            req.open('/delete/
+            req.open('POST', '/delete');
+            req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+            req.send(JSON.stringify({
+                'bot_id': this.getAttribute('bot_id'),
+                // TODO: this is not a very elegant way to get the token.
+                'access_token': document.getElementsByName('access_token')[0].value,
+            }));
         }
     }
 }
