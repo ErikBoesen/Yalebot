5f8a46273dfe9b763c1a4d3c6181d1bcc5e0fa7b
ErikBoesen <me@erikboesen.com>
2019-03-31 14:08:40 -0400

    Start implementing delete button functionality

--- static/scripts/controlpanel.js
+++ static/scripts/controlpanel.js
@@ -1,3 +1,12 @@
 document.getElementById('accept').onclick = function() {
     document.getElementById('add').disabled = !this.checked;
 }
+
+onclick = function(e) {
+    if (e.target.tagName == 'BUTTON' && e.target.classList.contains('delete')) {
+        if (confirm('Really delete bot?')) {
+            var req = new XMLHttpRequest();
+            req.open('/delete/
+        }
+    }
+}
