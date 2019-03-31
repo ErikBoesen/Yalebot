75c90de1922bb174a2e0a5ee871eff1216320bb1
ErikBoesen <me@erikboesen.com>
2019-03-31 15:06:57 -0400

    Re-enable removal of bot from database upon deletion

--- bot.py
+++ bot.py
@@ -256,8 +256,8 @@ def delete_bot():
     data = request.get_json()
     access_token = data["access_token"]
     req = requests.post(f"https://api.groupme.com/v3/bots/destroy?token={access_token}", json={"bot_id": data["bot_id"]})
-    print(req.text)
-    if False:
+    # TODO: this is a very messy way to make sure the request went through.
+    if req.ok and len(req.text) == 0:
         bot = Bot.query.get(bot_id=data["bot_id"])
         db.session.delete(bot)
         db.session.commit()
