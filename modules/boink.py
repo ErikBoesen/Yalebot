62d73dfcfa22863f14cf406dda21edfe34ca2fc0
ErikBoesen <me@erikboesen.com>
2019-04-01 10:11:00 -0400

    Stop fetching from GroupMe a list of members every time

--- modules/boink.py
+++ modules/boink.py
@@ -5,11 +5,13 @@ import random
 
 class Boink(Module):
     DESCRIPTION = "Make a match"
+    queues = {}
 
     def response(self, query, message):
         group_id = message["group_id"]
-        users = requests.get(f"https://api.groupme.com/v3/groups/{group_id}?token={self.ACCESS_TOKEN}").json()["response"]["members"]
-        users = [user["name"] for user in users]
-        name1 = random.choice(users)
-        name2 = random.choice(users)
+        if self.queues.get(group_id) is None or len(self.queues[group_id]) < 2:
+            users = requests.get(f"https://api.groupme.com/v3/groups/{group_id}?token={self.ACCESS_TOKEN}").json()["response"]["members"]
+            self.queues[group_id] = random.shuffle([user["name"] for user in users])
+        name1 = self.queues[group_id].pop()
+        name2 = self.queues[group_id].pop()
         return f"I choose... {name1} & {name2}! 💙"
