14c2573d92306b73c18247b53d1448262c5f0254
ErikBoesen <me@erikboesen.com>
2019-03-31 21:40:34 -0400

    Fix broken leave button

--- static/scripts/manager.js
+++ static/scripts/manager.js
@@ -7,7 +7,7 @@ onclick = function(e) {
             req.send(JSON.stringify({
                 'group_id': e.target.getAttribute('group_id'),
                 // TODO: this is not a very elegant way to get the token.
-                'access_token': document.getElementsByName('access_token')[0].value,
+                'access_token': document.getElementById('access_token').value,
             }));
             req.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
