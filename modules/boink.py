d0d482f417b55555b524578d696698a419a0e4ad
ErikBoesen <me@erikboesen.com>
2019-04-01 10:25:11 -0400

    Recognize that random.shuffle doesn't return anything

--- modules/boink.py
+++ modules/boink.py
@@ -11,7 +11,8 @@ class Boink(Module):
         group_id = message["group_id"]
         if self.queues.get(group_id) is None or len(self.queues[group_id]) < 2:
             users = requests.get(f"https://api.groupme.com/v3/groups/{group_id}?token={self.ACCESS_TOKEN}").json()["response"]["members"]
-            self.queues[group_id] = random.shuffle([user["name"] for user in users])
+            self.queues[group_id] = [user["name"] for user in users]
+            random.shuffle(self.queues[group_id])
         name1 = self.queues[group_id].pop()
         name2 = self.queues[group_id].pop()
         return f"I choose... {name1} & {name2}! 💙"
