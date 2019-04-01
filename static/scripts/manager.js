56c07b9c7f8fe90a0a957023405369405b13baf2
ErikBoesen <me@erikboesen.com>
2019-03-31 22:03:44 -0400

    Stop logging every background image

--- static/scripts/manager.js
+++ static/scripts/manager.js
@@ -4,6 +4,11 @@ onclick = function(e) {
             var req = new XMLHttpRequest();
             req.open('POST', '/delete');
             req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+            console.log({
+                'group_id': e.target.getAttribute('group_id'),
+                // TODO: this is not a very elegant way to get the token.
+                'access_token': document.getElementById('access_token').value,
+            });
             req.send(JSON.stringify({
                 'group_id': e.target.getAttribute('group_id'),
                 // TODO: this is not a very elegant way to get the token.
@@ -21,7 +26,6 @@ onclick = function(e) {
 var avatarURLInput = document.getElementsByName('avatar_url')[0],
     avatarPreview = document.getElementById('avatar_preview');
 function previewAvatar() {
-    console.log(avatarURLInput.value);
     avatarPreview.style.backgroundImage = 'url(' + avatarURLInput.value + ')';
 }
 previewAvatar();
