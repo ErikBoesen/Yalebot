be845a4be910c8232c9462734f9cf438c3de9769
ErikBoesen <me@erikboesen.com>
2019-04-01 17:53:04 -0400

    Limit number of ribbits you can do with hema

--- modules/hema.py
+++ modules/hema.py
@@ -4,10 +4,12 @@ import random
 
 class Hema(Module):
     DESCRIPTION = "Ribbit"
+    MAX_RIBBITS = 500
 
     def response(self, query, message):
         try:
             repetitions = int(query)
+            repetitions = min(repetitions, self.MAX_RIBBITS)
         except ValueError:
             repetitions = random.randint(5, 30)
         return " ".join(["ribbit"] * repetitions)
