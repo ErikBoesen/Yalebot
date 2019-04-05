4028989d26f12de418aec468e435a45205cd63ef
ErikBoesen <me@erikboesen.com>
2019-04-05 08:47:38 -0400

    Read image into tesseract in module

--- modules/battery.py
+++ modules/battery.py
@@ -1,5 +1,21 @@
+from .base import Module, ImageUploader
 from PIL import Image
+from io import BytesIO
 from pytesseract import image_to_string
+# TODO: This is so complicated for literally just reading an image from a URL
+from skimage import io
 
-print(image_to_string(Image.open('/home/erik/ss.png')))
 
+class Truman(Module, ImageUploader):
+    DESCRIPTION = "Is your battery sinfully low?"
+
+    def response(self, query, message):
+        source_url = self.get_source_url(message)
+
+        image = io.imread(source_url)[:, :, :3]A
+        pil_image = Image.fromarray(image)
+        image_to_string(pil_image)
+
+        output = BytesIO()
+        pil_image.save(output, format="JPEG")
+        return "", self.upload_image(output.getvalue())
