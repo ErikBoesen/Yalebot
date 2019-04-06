044ced447fd932cde4a1628c3835619916f53922
ErikBoesen <me@erikboesen.com>
2019-04-06 12:47:05 -0400

    Correct for % being read as 0/0 by OCR

--- modules/truman.py
+++ modules/truman.py
@@ -5,13 +5,14 @@ import ocr_space
 
 class Truman(Module, ImageUploader):
     DESCRIPTION = "Is your battery sinfully low?"
-    PERCENTAGE_RE = re.compile(r"(\d+)%")
+    PERCENTAGE_RE = re.compile(r"(\d+) *%")
     api = ocr_space.OCRSpace("helloworld")
 
     def response(self, query, message):
         source_url = self.get_source_url(message)
         string = self.api.ocr_url(source_url)["ParsedResults"][0]["ParsedText"]
         print("Image text: " + string)
+        string = string.replace("0/0")
         percentages = self.PERCENTAGE_RE.search(string).groups()
         if len(percentages) > 0:
             percentage = percentages[0]
