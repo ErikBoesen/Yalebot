acac2d656c661f04aa322543a4f68b11f38e6309
ErikBoesen <me@erikboesen.com>
2019-04-06 12:19:06 -0400

    Use ocr_space module for truman

--- modules/truman.py
+++ modules/truman.py
@@ -1,22 +1,16 @@
 from .base import Module, ImageUploader
-from PIL import Image
-from io import BytesIO
 import re
-from pytesseract import image_to_string
-# TODO: This is so complicated for literally just reading an image from a URL
-from skimage import io
+import ocr_space
 
 
 class Truman(Module, ImageUploader):
     DESCRIPTION = "Is your battery sinfully low?"
     PERCENTAGE_RE = re.compile(r"(\d+)%")
+    api = ocr_space.OCRSpace("helloworld")
 
     def response(self, query, message):
         source_url = self.get_source_url(message)
 
-        image = io.imread(source_url)[:, :, :3]
-        pil_image = Image.fromarray(image)
-        string = image_to_string(pil_image)
         percentages = self.PERCENTAGE_RE.search(string).groups()
         if len(percentages) > 0:
             percentage = percentages[0]
