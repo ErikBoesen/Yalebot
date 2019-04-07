d2b91c8bbc131e4276d180c92abab43969f6afb8
ErikBoesen <me@erikboesen.com>
2019-04-07 19:48:27 -0400

    Fix translate not being defined

--- modules/nato.py
+++ modules/nato.py
@@ -2,7 +2,6 @@ from .base import Module
 
 
 class NATO(Module):
-    DESCRIPTION = translate("It good")
     dictionary = {"A":"Alpha", "B":"Bravo","C":"Charlie", "D":"Delta", "E":"Echo", "F":"Foxtrot", "G":"Golf","H":"Hotel", "I":"India", "J":"Juliet", "K":"Kilo", "L":"Lima", "M":"Mike", "N":"November", "O":"Oscar", "P":"Papa", "Q":"Quebec", "R":"Romeo", "S":"Sierra", "T":"Tango", "U":"Uniform", "V":"Victor", "W":"Whiskey", "X":"Xray", "Y":"Yankee", "Z":"Zulu"}
 
     def convert(self, char):
@@ -16,3 +15,5 @@ class NATO(Module):
 
     def response(self, query, message):
         return self.translate(query)
+
+    DESCRIPTION = translate("It good")
