dcce0438ccf06524057f04535343da70da3bb990
ErikBoesen <me@erikboesen.com>
2019-04-07 20:08:38 -0400

    Add !chike

--- bot.py
+++ bot.py
@@ -33,6 +33,7 @@ static_commands = {
     "bang": ("", "https://i.groupme.com/720x1440.png.c76127a21867451093edd11bbb09d75d.large"),
     "oof": ("Oh?", "https://i.groupme.com/1500x1125.jpeg.9b2c341aa9854831ab2525d7e21e974a.large"),
     "tease": ("", "https://i.groupme.com/936x1246.jpeg.d0d60970b329415cac1d7a1825a783a7.large"),
+    "chike": ("", "https://i.groupme.com/1021x1400.jpeg.70192657c76745ab809357d0512d4951.large"),
 }
 
 commands = {
