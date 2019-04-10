166692026cb0d972a3ee71154fda43db4d7b9d58
dominiccoletti <49297507+dominiccoletti@users.noreply.github.com>
2019-04-09 21:39:24 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,6 +35,7 @@ static_commands = {
     "tease": ("", "https://i.groupme.com/936x1246.jpeg.d0d60970b329415cac1d7a1825a783a7.large"),
     "chike": ("", "https://i.groupme.com/1021x1400.jpeg.70192657c76745ab809357d0512d4951.large"),
     "pressed": ("", "https://i.groupme.com/540x719.jpeg.2229bdb9f15247a7a112ac0be95e065a.large"),
+    "alex": ("", "https://i.groupme.com/1021x1400.jpeg.7bd963eae3824435bf749b96ce4fd84a.large"),
 }
 
 commands = {
