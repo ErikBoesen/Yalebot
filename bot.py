d4d03d0bb38636f97ca9c0c95cffad722ca14114
ErikBoesen <me@erikboesen.com>
2019-04-18 12:58:23 -0400

    Add !ohno

--- bot.py
+++ bot.py
@@ -39,6 +39,7 @@ static_commands = {
     "bddschedule": "https://bulldogdays2019.sched.com/",
     "flex": "👮🏽🚨🚔 PULL OVER 👮🏽🚨🚔\n\n😤Put your hands behind your back😤\n\n🗣I'm taking you into custody🗣\n\n📝And registering you as a📝\n\n🔥😩FLEX OFFENDER😩🔥",
     "amma": ("", "https://i.groupme.com/714x456.jpeg.66fb9e9dacab4cd9b860b084eceff282.large"),
+    "ohno": ("", "https://i.groupme.com/1280x720.jpeg.f7c11a529a3b4a7195f71fa6be5ebfef.large"),
 }
 
 commands = {
