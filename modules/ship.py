36ad0cead314f5509a32cade7c37a48bd84b21d0
ErikBoesen <me@erikboesen.com>
2019-04-19 18:03:51 -0400

    Flip around ship names

--- modules/ship.py
+++ modules/ship.py
@@ -4,11 +4,13 @@ import requests
 
 class Ship(Module):
     ARGC = 1
+    ENDPOINT = "https://couplenamegenerator.com/combine"
 
     def response(self, query, message):
         names = query.split()
         if len(names) != 2:
             return "Please provide two names."
-        result = requests.post("https://couplenamegenerator.com/combine", data={"a1": names[0], "a2": names[1]}).json()
+        result = requests.post(self.ENDPOINT, data={"a1": names[0], "a2": names[1].lower()}).json()
+        result += requests.post(self.ENDPOINT, data={"a1": names[1], "a2": names[2].lower()}).json()
         return ", ".join(result)
 
