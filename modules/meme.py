7df4c58cc90a82b60504f3842c87ca629f4bdd5f
ErikBoesen <me@erikboesen.com>
2019-04-22 18:07:25 -0400

    Add pooh meme

--- modules/meme.py
+++ modules/meme.py
@@ -83,6 +83,10 @@ class Meme(Module, ImageUploader):
             "load": (
                 {"position": (128, 128), "wrap": 18, "center_vertical": True},
             ),
+            "pooh": (
+                {"position": (563, 141), "center_vertical": True},
+                {"position": (563, 441), "center_vertical": True},
+            ),
         }
         self.templates["yaledrake"] = self.templates["drake"]
         self.DESCRIPTION = "Generate memes! List the desired template, and then captions each on a new line. " + self.list_templates()
