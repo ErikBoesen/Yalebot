e2a3b6b28da20c15893d5e913752e8b60b495aa8
ErikBoesen <me@erikboesen.com>
2019-04-23 18:16:30 -0400

    Re-add heart masking for carlos

--- modules/carlos.py
+++ modules/carlos.py
@@ -28,6 +28,7 @@ class Carlos(Module, ImageUploader):
             processed_heart = heart.resize((heart_size, heart_size), Image.ANTIALIAS).rotate(random.randint(0, 360), expand=True)
             processed_width, processed_height = processed_heart.size
             background.paste(processed_heart,
-                             (int(random.random() * (image_width - processed_width)), int(random.random() * (image_height - processed_height))))
+                             (int(random.random() * (image_width - processed_width)), int(random.random() * (image_height - processed_height))),
+                             processed_heart)
 
         return "", self.upload_pil_image(background)
