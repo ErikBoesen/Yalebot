e554836f30f5362e83f017e188b3082766c1b1d1
ErikBoesen <me@erikboesen.com>
2019-04-24 12:14:51 -0400

    Add debug logging when sending card chioce to endpoint

--- static/scripts/cah.js
+++ static/scripts/cah.js
@@ -6,13 +6,16 @@ onclick = function(e) {
         req.open("POST", location.href);
         req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         var url = new URL(location.href);
-        req.send(JSON.stringify({
+        var data = {
             "access_token": url.searchParams.get("access_token"),
             "card_index": cardIndex,
-        }));
+        };
+        req.send(JSON.stringify(data));
         req.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 location.reload();
+            } else {
+                console.log("Failed to report data.", data);
             }
         };
     }
