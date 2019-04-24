d60a4d69db4d26c5342432fbd5ec074f51e0d572
ErikBoesen <me@erikboesen.com>
2019-04-24 10:01:24 -0400

    Remove old image downloading code from crist

--- modules/crist.py
+++ modules/crist.py
@@ -1,9 +1,6 @@
 from .base import Module, ImageUploader
 from PIL import Image
 import random
-from io import BytesIO
-# TODO: This is so complicated for literally just reading an image from a URL
-from skimage import io
 
 
 class Crist(Module, ImageUploader):
@@ -12,8 +9,7 @@ class Crist(Module, ImageUploader):
 
     def response(self, query, message):
         source_url = self.get_source_url(message)
-        image = io.imread(source_url)[:, :, :3]
-        portrait = Image.fromarray(image)
+        portrait = self.pil_from_url(source_url)
         background = self.heaven.copy()
 
         portrait_natural_width, portrait_natural_height = portrait.size
