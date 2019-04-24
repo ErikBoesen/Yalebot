f2792b0952ee65071604bb6cfc3a9219e0d3f6a3
ErikBoesen <me@erikboesen.com>
2019-04-24 17:14:40 -0400

    Allow mentioning the first partner in a boink

--- modules/boink.py
+++ modules/boink.py
@@ -1,11 +1,20 @@
 from .base import Module
 import requests
 import random
+import re
 
 
 class Boink(Module):
     DESCRIPTION = "Make a match"
     queues = {}
+
+    def get_mention(self, query):
+        """
+        Find the name of anyone mentioned in message.
+        """
+        match = re.search(r'@.+', query)
+        if match:
+            return match.group().lstrip('@')
 
     def response(self, query, message):
         group_id = message["group_id"]
@@ -13,6 +22,7 @@ class Boink(Module):
             users = requests.get(f"https://api.groupme.com/v3/groups/{group_id}?token={self.ACCESS_TOKEN}").json()["response"]["members"]
             self.queues[group_id] = [user["name"] for user in users]
             random.shuffle(self.queues[group_id])
-        name1 = self.queues[group_id].pop()
+        mention = self.get_mention(query)
+        name1 = mention or self.queues[group_id].pop()
         name2 = self.queues[group_id].pop()
         return f"I choose... {name1} & {name2}! 💙"
