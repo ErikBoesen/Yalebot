25d49e873962670c6ab79af5566bdcbb975dff17
ErikBoesen <me@erikboesen.com>
2019-04-27 16:29:01 -0400

    Fix record help periods

--- modules/record.py
+++ modules/record.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 
 class Record(Module):
-    DESCRIPTION = "Search for articles on the Yale Record, Yale's foremost satire magazine."
+    DESCRIPTION = "Search for articles on the Yale Record, Yale's foremost satire magazine"
     ARGC = 1
 
     def response(self, query, message):
