ca143f00028f592ae0cf67ff84fa89b5c89c7a5d
ErikBoesen <me@erikboesen.com>
2019-04-28 16:21:02 -0400

    Add Black Yale to groups

--- modules/groups.py
+++ modules/groups.py
@@ -8,6 +8,7 @@ class Groups(Module):
         "Yale Prom": "47397377/6SQ6Z9",
         "Robotics Yalies": "48299309/LfsRP2eP",
         "Directed Studies": "49954511/RJGSTgEU",
+        "Black Yale 2023": "49447961/dUZwOQEp",
 
         "DMV Region": "46655481/kpn04v",
     }
