48884638392a6ac6a4e6ed6e6c395e89a0a632c7
Victor Del Carpio <vhdelcg@gmail.com>
2019-04-29 12:00:22 -0400

    Update youtube.py

--- modules/youtube.py
+++ modules/youtube.py
@@ -8,6 +8,6 @@ class YouTube(Module):
     ARGC = 1
 
     def response(self, query, message):
-        url = f"https://www.youtube.com/results?search_query={query.replace(' ', '%20').lower()}"
+        url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+').lower()}"
         bs = BeautifulSoup(requests.get(url).text, "html.parser")
         return f"https://www.youtube.com{list(map(lambda x: x.get("href"), bs.find_all("a")))[44]}"
