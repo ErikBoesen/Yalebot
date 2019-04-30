4aa51f8cd42b65b8972eda61e7efa91998570171
ErikBoesen <me@erikboesen.com>
2019-04-30 17:50:10 -0400

    Use single quotes consistently in tic tac toe

--- modules/tictactoe.py
+++ modules/tictactoe.py
@@ -4,24 +4,24 @@ from .base import Module
 class TicTacToe(Module):
     DESCRIPTION = "Play a friendly game of tic tac toe"
     ARGC = 1
-    players = [''] * 2
-    board = [' '] * 9
-    movements = {'a1': 0, 'a2': 1, 'a3': 2,
-                 'b1': 3, 'b2': 4, 'b3': 5,
-                 'c1': 6, 'c2': 7, 'c3': 8}
-    turn = True    
-    
+    players = [""] * 2
+    board = [" "] * 9
+    movements = {"a1": 0, "a2": 1, "a3": 2,
+                 "b1": 3, "b2": 4, "b3": 5,
+                 "c1": 6, "c2": 7, "c3": 8}
+    turn = True
+
     def clear(self):
-        self.players = [''] * 2
-        self.board = [' '] * 9
+        self.players = [""] * 2
+        self.board = [" "] * 9
         self.turn = True
-    
+
     def board(self):
-        pboard = '|'.join(self.board[:3]) + '\n——————\n'
-        pboard += '|'.join(self.board[3:6]) + '\n——————\n'
-        pboard += '|'.join(self.board[6:])
+        pboard = "|".join(self.board[:3]) + "\n——————\n"
+        pboard += "|".join(self.board[3:6]) + "\n——————\n"
+        pboard += "|".join(self.board[6:])
         return pboard
-    
+
     def check(self):
         #TODO: find a better way to check for three in a row
         check = [self.board[0:3], self.board[3:6], self.board[6:],
@@ -31,44 +31,44 @@ class TicTacToe(Module):
                  [self.board[0], self.board[4], self.board[8]],
                  [self.board[2], self.board[4], self.board[6]]]
         for arr in check:
-            if arr[:3] == ['x'] * 3:
+            if arr[:3] == ["x"] * 3:
                 self.clear()
-                return f'{self.players[0]} wins!'
-            elif arr[:3] == ['o'] * 3:
+                return f"{self.players[0]} wins!"
+            elif arr[:3] == ["o"] * 3:
                 self.clear()
-                return f'{self.players[1]} wins!'
-        return ''
-    
+                return f"{self.players[1]} wins!"
+        return ""
+
     def response(self, query, message):
         arguments = query.split()
         command = arguments.pop(0).lower()
         name = message["name"]
-        if command == 'join':
-            if self.players[0] == '':
+        if command == "join":
+            if self.players[0] == "":
                 self.players[0] = name
                 return f"{name} has joined, waiting on a second player"
-            elif self.players[1] == '':
+            elif self.players[1] == "":
                 self.players[1] = name
                 return [f"{name} has joined, ready to play", self.board()]
             else:
                 return f"Game full. {self.players[0]} & {self.players[1]} are playing!"
-        elif command == 'end':
+        elif command == "end":
             self.clear()
-        elif command == 'help':
-            desc = 'Possible commands: help, join, end. Positions:\n'
-            desc += '|'.join(['a1','a2','a3']) + '\n——————\n'
-            desc += '|'.join(['b1','b2','b3']) + '\n——————\n'
-            desc += '|'.join(['c1','c2','c3'])
+        elif command == "help":
+            desc = "Possible commands: help, join, end. Positions:\n"
+            desc += "|".join(["a1","a2","a3"]) + "\n——————\n"
+            desc += "|".join(["b1","b2","b3"]) + "\n——————\n"
+            desc += "|".join(["c1","c2","c3"])
             return desc
         elif command in self.movements:
             loc = self.movements[command]
             if self.turn and name == self.players[0]:
                 self.turn = False
-                self.board[loc] = 'x'
+                self.board[loc] = "x"
             elif not self.turn and name == self.players[1]:
                 self.turn = True
-                self.board[loc] = 'o'
-            if self.check() != '':
+                self.board[loc] = "o"
+            if self.check() != "":
                 return self.check()
             return self.board()
         else:
