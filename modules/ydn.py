555c2b8762f5e96d0c5d478252d58b4eea03a9bd
ErikBoesen <me@erikboesen.com>
2019-04-30 16:10:20 -0400

    Use clearer syntax for YaleNews/YDN

--- modules/ydn.py
+++ modules/ydn.py
@@ -8,11 +8,12 @@ class YaleNews(Module):
     ARGC = 1
 
     def response(self, query, message):
-        url = f"https://news.yale.edu/search?sort=created&order=desc&search_api_views_fulltext={query}"
+        url = "https://news.yale.edu/search?sort=created&order=desc&search_api_views_fulltext={query}"
         soup = BeautifulSoup(requests.get(url).text, "html.parser")
-        link = list(map(lambda x: x.get("href"), soup.find_all("a")))[22]
+        # TODO: protect against exact link index being changed
+        link = [link.get("href") for link in soup.find_all("a")][22]
         # if typo comes up, searches the suggested keyword
         if "/search?" in link:
-            soup = BeautifulSoup(requests.get(f"https://news.yale.edu{link}").text, "html.parser")
+            soup = BeautifulSoup(requests.get("https://news.yale.edu" + link).text, "html.parser")
             link = list(map(lambda x: x.get("href"), soup.find_all("a")))[22]
         return f"https://news.yale.edu{link}"
