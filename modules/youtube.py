53bd678b030b889dd249871c12002288477ab83a
ErikBoesen <me@erikboesen.com>
2019-04-30 17:54:30 -0400

    Clean up youtube syntax

--- modules/youtube.py
+++ modules/youtube.py
@@ -8,6 +8,7 @@ class YouTube(Module):
     ARGC = 1
 
     def response(self, query, message):
-        url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+').lower()}"
+        url = "https://www.youtube.com/results?search_query=" + query.replace(" ", "+")
         bs = BeautifulSoup(requests.get(url).text, "html.parser")
-        return f"https://www.youtube.com{list(map(lambda x: x.get("href"), bs.find_all("a")))[44]}"
+        link = bs.find_all("a")[44]["href"]
+        return "https://www.youtube.com" + link
