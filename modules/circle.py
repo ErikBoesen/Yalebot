99237ebfbdb3f6bd1f4bee90520b42c284a5de6e
ErikBoesen <me@erikboesen.com>
2019-05-05 15:55:54 -0400

    Fix undefined group_id in circle

--- modules/circle.py
+++ modules/circle.py
@@ -7,6 +7,7 @@ class Circle(Module):
     DESCRIPTION = "Generate a compliment circle"
 
     def response(self, query, message):
+        group_id = message["group_id"]
         users = requests.get(f"https://api.groupme.com/v3/groups/{group_id}?token={self.ACCESS_TOKEN}").json()["response"]["members"]
         users = [user["name"] for user in users]
         random.shuffle(users)
