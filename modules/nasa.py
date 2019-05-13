9a3e7a8b2fbd6663fd1836f0ec350d8aa6f0639c
ErikBoesen <me@erikboesen.com>
2019-05-13 18:04:11 -0400

    Fix subscripting of NASA photo

--- modules/nasa.py
+++ modules/nasa.py
@@ -11,5 +11,5 @@ class NASA(Module):
         photo = requests.get("https://api.nasa.gov/planetary/apod?api_key=" + (os.environ.get("APOD_KEY") or "DEMO_KEY")).json()
 
         # Send URL for image along with image title
-        return ["NASA Image of the Day " + photo["date"] + "\n\n" + photo["description"],
+        return ["NASA Image of the Day " + photo["date"] + "\n\n" + photo["explanation"],
                 photo["hdurl"]]
