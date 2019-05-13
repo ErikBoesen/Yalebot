96c29dbb728e11226afe06aaf34e2bc733488818
ErikBoesen <me@erikboesen.com>
2019-05-13 17:57:23 -0400

    Send description with NASA image

--- modules/nasa.py
+++ modules/nasa.py
@@ -8,7 +8,8 @@ class NASA(Module):
 
     def response(self, query, message):
         # Get JSON data from NASA APOD API
-        photo = requests.get("https://api.nasa.gov/planetary/apod?api_key=%s" % (os.environ.get("APOD_KEY") or "DEMO_KEY")).json()
+        photo = requests.get("https://api.nasa.gov/planetary/apod?api_key=" + (os.environ.get("APOD_KEY") or "DEMO_KEY")).json()
 
         # Send URL for image along with image title
-        return photo["url"]
+        return ["NASA Image of the Day " + photo["date"] + "\n\n" + photo["description"],
+                photo["hdurl"]]
