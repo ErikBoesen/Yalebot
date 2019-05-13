9788fe3899552cabc15a40636d89ffe530caf04e
ErikBoesen <me@erikboesen.com>
2019-05-13 17:36:05 -0400

    Reset to old YouTube code

--- modules/youtube.py
+++ modules/youtube.py
@@ -8,7 +8,10 @@ class YouTube(Module):
     ARGC = 1
 
     def response(self, query, message):
-        url = "https://www.youtube.com/results?search_query=" + query.replace(" ", "+")
-        bs = BeautifulSoup(requests.get(url).text, "html.parser")
-        link = bs.find_all("a")[44]["href"]
-        return "https://www.youtube.com" + link
+        url = "https://www.youtube.com/results?search_query="
+        bs = BeautifulSoup(requests.get(url + query.replace(" ", "+").lower()).text, "html.parser")
+        for link in bs.find_all("a"):
+            result = link.get("href")
+            if result.startswith("/watch"):
+                break
+        return "https://www.youtube.com" + result
