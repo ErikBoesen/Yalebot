8ae823af164410571acc12e1fa7f3014a3dab197
Victor Del Carpio <vhdelcg@gmail.com>
2019-05-13 17:35:05 -0400

    Update conversationstarter.py
    #118

--- modules/conversationstarter.py
+++ modules/conversationstarter.py
@@ -4,9 +4,14 @@ import requests
 
 class ConversationStarter(Module):
     DESCRIPTION = "Generate an icebreaker question"
+    responses = []
+    
+    def fill_queue(self):
+        for x in range(10):
+            question = requests.post("https://www.conversationstarters.com/random.php").text
+            self.responses.append(question[39:])
 
     def response(self, query, message):
-        question = requests.post("https://www.conversationstarters.com/random.php").text
-        # Trim off <img> tag from start
-        question = question[39:]
-        return question
+        if len(self.responses) == 0:
+            self.fill_queue()
+        return self.responses.pop()
