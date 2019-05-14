982efba74f9b5799d774501bf8cda771f97c9529
ErikBoesen <me@erikboesen.com>
2019-05-14 16:09:28 -0400

    Limit size of JPEG images very aggressively

--- modules/jpeg.py
+++ modules/jpeg.py
@@ -9,6 +9,7 @@ class JPEG(ImageModule):
     def response(self, query, message):
         source_url = self.get_source_url(message)
         image = self.pil_from_url(source_url)
+        image = self.limit_image_size(image, width=400)
         output = BytesIO()
         image.save(output, format="JPEG", quality=2)
         return ("", self.upload_image(output.getvalue()))
