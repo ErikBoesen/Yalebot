59808f7383a66e245190fd0c030b09fb3f862251
ErikBoesen <me@erikboesen.com>
2019-05-15 14:21:24 -0400

    Add FSY chat to groups

--- modules/groups.py
+++ modules/groups.py
@@ -11,6 +11,7 @@ class Groups(Module):
         "Black Yale 2023": "49447961/dUZwOQEp",
         "First Generation students": "49418581/byuFtN4q",
         "LGBT+": "49376932/0wl2GN9F",
+        "FSY": "50473402/gMWpQ8WK",
 
         "New York City": "46816234/B3PBXk",
         "DMV": "46655481/kpn04v",
