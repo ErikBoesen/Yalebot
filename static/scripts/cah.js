7a4eee9279ee57ad2c8b6374adac91d9728cb614
ErikBoesen <me@erikboesen.com>
2019-05-18 13:29:13 -0400

    Clean up CAH JavaScript code a lot

--- static/scripts/cah.js
+++ static/scripts/cah.js
@@ -1,41 +1,44 @@
+// Retrieve access token from URL parameter
 var access_token = (new URL(location.href)).searchParams.get("access_token");
 
+// Initiate connection to websocket, and listen for connection
 var socket = io.connect(location.protocol + "//" + location.host);
 socket.on("connect", function(data) {
     console.log(data.connected ? "Connected" : "Disconnected", data);
     socket.emit("cah_connect", {"access_token": access_token});
 });
 
+// Create card elements easily
 function buildCard(color, content) {
     var card = document.createElement("div");
-    card.classList.add("card", color);
+    card.classList.add(color, "card");
     card.textContent = content;
     return card;
 }
 
-var selection = document.getElementById("selection"); // TODO: there might be others
+// Card list from which Czar chooses
+var selection = document.getElementById("selection");
+// Your own cards, from which you choose when not Czar
 var hand = document.getElementById("hand");
 function fillRow(row, cards) {
-    row.innerHTML = ""; // you ignoramus, do this right
+    // Empty whatever is in the row first
+    while (row.firstChild) row.removeChild(row.firstChild);
     for (card of cards) {
         row.appendChild(buildCard("white", card));
     }
 }
-function fillSelection(cards) {
-    fillRow(selection, cards);
-}
-function fillHand(cards) {
-    fillRow(hand, cards);
-}
+function fillSelection(cards) { fillRow(selection, cards); }
+function      fillHand(cards) { fillRow(hand, cards); }
+
 socket.on("cah_ping", function(data) {
-    // Should we use bracket notation? Should we use camelCase because this is JavaScript? Should we delete this entire bloated bot and git commit seppuku? Maybe
+    // If user hasn't joined a game, warn them.
     if (!data.joined) {
         document.getElementById("warning").textContent = "You're not in a game. Type !cah join in the group first.";
+        document.getElementById("cah").style.display = "none";
         return;
     }
 
-    // Again... address this in a more effective way
-    document.getElementsByClassName("black")[0].textContent = data.black_card;
+    document.getElementById("black").textContent = data.black_card;
 
     if (data.is_czar) {
         document.getElementById("czar").textContent = "You are Card Czar this round. Please select a card.";
@@ -44,7 +47,6 @@ socket.on("cah_ping", function(data) {
         fillHand(data.hand);
     } else {
         // TODO: there's nothing stopping anyone from submitting their own cards on the server-side, just that they won't be shown.
-        // You lazy idiot.
         document.getElementById("czar").textContent = "";
         fillHand(data.hand);
     }
@@ -57,10 +59,10 @@ onclick = function(e) {
         // TEMPORARY for debugging
         is_czar =  (e.target.parentNode.id == "selection");
 
-        var url = new URL(location.href);
         var data = {
-            "access_token": url.searchParams.get("access_token"),
+            "access_token": access_token,
             "card_index": cardIndex,
+            // TEMPORARY
             "is_czar": is_czar,
         };
         socket.emit("cah_selection", data);
