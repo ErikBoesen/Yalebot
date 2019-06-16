28507ed854c0e7b421805fbd9bed1bbd241525b4
ErikBoesen <me@erikboesen.com>
2019-06-16 19:00:52 -0400

    feat: add dining module

--- modules/dining.py
+++ modules/dining.py
@@ -4,4 +4,50 @@ from yaledining import YaleDining
 
 class Dining(Module):
     DESCRIPTION = "Get information about Yale Dining services, closures, menus, etc."
-    ARGC = 1
+    dining = YaleDining()
+    type_emoji = {
+        "Residential": "🏠",
+        "Retail": "💰",
+    }
+
+    def get_managers(location):
+        managers = []
+        num_managers = 0
+        while num_managers < 4:
+            num_managers += 1
+            manager = (location[f"MANAGER{num_managers}NAME"],
+                       location[f"MANAGER{num_managers}EMAIL"])
+            if manager == (None, None):
+                break
+            managers.append(manager)
+        return ", ".join([f"{name} ({email})" for name, email in managers])
+
+    def response(self, query, message):
+        locations = dining.get_locations()
+        response = ""
+        if query:
+            desired_location =  None
+            for location in locations:
+                if args.location == location["DININGLOCATIONNAME"]:
+                    desired_location = location
+            if desired_location is None:
+                return("Unknown location name.")
+            response += "-" * 3 + desired_location["DININGLOCATIONNAME"] + "-" * 3 + "\n"
+            response += "Address: " + "{address} ({coordinates})\n".format(address=location["ADDRESS"],
+                                                                           coordinates=location["GEOLOCATION"])
+            response += "Phone: " + location["PHONE"] + "\n"
+            response += "Managers: " + self.get_managers(location) + "\n"
+            menus = dining.get_menus(desired_location["ID_LOCATION"])
+            response += "\n" * 2
+            if menus:
+                response += "Menu support coming soon!"
+            else:
+                response += "No menu is currently available."
+        else:
+            for location in locations:
+                is_open = not bool(location["ISCLOSED"])
+                response += "- {emoji} {name} ({status})".format(emoji=self.type_emoji[location["TYPE"]],
+                                                                 name=location["DININGLOCATIONNAME"],
+                                                                 status="Open, {capacity}%".format(capacity=10 * location["CAPACITY"])
+                                                                    if is_open else "Closed")
+        return response
