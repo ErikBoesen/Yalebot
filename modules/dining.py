e58dac8ce67c8504f8bcb7cfb83a12bb2d51edc6
ErikBoesen <me@erikboesen.com>
2019-06-17 21:57:30 -0400

    feat: use simpler yaledining syntax

--- modules/dining.py
+++ modules/dining.py
@@ -10,53 +10,34 @@ class Dining(Module):
         "Retail": "💰",
     }
 
-    def get_managers(self, location):
-        managers = []
-        num_managers = 0
-        while num_managers < 4:
-            num_managers += 1
-            manager = (location[f"MANAGER{num_managers}NAME"],
-                       location[f"MANAGER{num_managers}EMAIL"])
-            if manager == (None, None):
-                break
-            managers.append(manager)
-        return ", ".join([f"{name} ({email})" for name, email in managers])
-
     def capacity_bar(self, capacity: int):
         return ('▇' * capacity) + ('░' * (10 - capacity))
 
     def response(self, query, message):
-        locations = self.api.get_locations()
         response = ""
         if query:
-            location = None
-            for location_candidate in locations:
-                if query == location_candidate["DININGLOCATIONNAME"]:
-                    location = location_candidate
+            location = self.api.location(name=query)
             if location is None:
                 return f"Unknown location name '{query}'."
-            response += ("-" * 3) + location["DININGLOCATIONNAME"] + ("-" * 3) + "\n"
-            # TODO: repetition
-            is_open = not bool(location["ISCLOSED"])
-            # TODO: this sucks lol
-            response += "Open: " + ("Yes" if is_open else "No") + "\n"
-            if is_open:
-                response += "Capacity: {capacity}% ".format(capacity=location["CAPACITY"] * 10) + self.capacity_bar(location["CAPACITY"]) + "\n"
-            response += "Type: " + location["TYPE"] + " " + self.type_emoji[location["TYPE"]] + "\n"
-            response += "Address: " + "{address} ({coordinates})\n".format(address=location["ADDRESS"],
-                                                                           coordinates=location["GEOLOCATION"])
-            response += "Phone: " + location["PHONE"] + "\n"
-            response += "Managers: " + self.get_managers(location) + "\n"
-            menus = self.api.get_menus(location["ID_LOCATION"])
+            response += ("-" * 3) + location.name + ("-" * 3) + "\n"
+            response += "Open: " + ("Yes" if locaion.open else "No") + "\n"
+            if location.open:
+                response += "Capacity: {capacity}% ".format(capacity=location.percent_capacity) + self.capacity_bar(location.percent_capacity) + "\n"
+            response += "Type: " + location.type + " " + self.type_emoji[location.type] + "\n"
+            response += "Address: " + "{address} ({coordinates})\n".format(address=location.address,
+                                                                           coordinates=location.geolocation)
+            response += "Phone: " + location.phone + "\n"
+            response += "Managers: " + ", ".join([f"{manager.name} ({manager.location})" for manager in managers]) + "\n"
+            menus = location.menus
             response += "\n"
             if menus:
                 response += "Menu support coming soon!"
             else:
                 response += "No menu is currently available."
         else:
+            locations = self.api.locations()
             for location in locations:
-                is_open = not bool(location["ISCLOSED"])
                 response += "- {emoji} {name} ({status})\n".format(emoji=self.type_emoji[location["TYPE"]],
-                                                                   name=location["DININGLOCATIONNAME"],
-                                                                   status=("Open" + ((", {capacity}% capacity".format(capacity=10 * location["CAPACITY"]) + self.capacity_bar(location["CAPACITY"])) if location.get("CAPACITY") is not None else "")) if is_open else "Closed")
+                                                                   name=location.name,
+                                                                   status=("Open" + ((", {capacity}% capacity".format(capacity=location.percent_capacity) + self.capacity_bar(location.capacity)) if location.capacity is not None else "")) if location.open else "Closed")
         return response
