5dc49a375b56d042c3bc6d49f070dd2b3cf513e9
ErikBoesen <me@erikboesen.com>
2019-06-24 20:31:27 -0400

    Clean up logic for poem

--- modules/poem.py
+++ modules/poem.py
@@ -9,25 +9,23 @@ class Poem(Module):
     def __init__(self):
         super().__init__()
         with open("resources/poetry.txt", "r") as f:
-            words = f.read().split()
+            words = f.read().replace("\"", "").split()
         index = 1
-        chain = {}
-        # This loop creates a dicitonary called "chain". Each key is a word, and the value of each key
-        # is an array of the words that immediately followed it.
-        for word in self.poems[index:]:
-            key = self.poems[index - 1]
-            if key in chain:
-                chain[key].append(word)
+        self.chain = {}
+        for word in words[index:]:
+            key = words[index - 1]
+            if key in self.chain:
+                self.chain[key].append(word)
             else:
-                chain[key] = [word]
+                self.chain[key] = [word]
             index += 1
 
     def response(self, query, message):
-        first_word = random.choice(list(chain.keys()))
-        message = first_word.capitalize()
+        word = random.choice(list(self.chain.keys()))
+        message = word.capitalize()
 
         for _ in range(100):
-            word = random.choice(chain[word])
+            word = random.choice(self.chain[word])
             message += ' ' + word
 
         return message
