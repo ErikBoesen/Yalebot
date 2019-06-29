69ce7a3480a4a5fe0156d90791510c88a5d5698f
ErikBoesen <me@erikboesen.com>
2019-06-29 14:27:10 -0400

    Test static commands

--- tests.py
+++ tests.py
@@ -2,10 +2,19 @@ import unittest
 import bot
 
 
-class ProcessMessage(unittest.TestCase):
+class MessageProcessing(unittest.TestCase):
+    def _message(self, text, response):
+        if type(response) != list:
+            response = [response]
+        message = bot.Message(text=text)
+        self.assertEqual(bot.process_message(message), response)
+
     def test_empty(self):
-        message = bot.Message(text="This message shouldn't trigger a response.")
-        self.assertEqual(bot.process_message(message), [])
+        self._message("This shouldn't trigger anything.", [])
+
+    def test_static(self):
+        for key in bot.static_commands:
+            self._message(bot.PREFIX + key, bot.static_commands[key])
 
 
 if __name__ == "__main__":
