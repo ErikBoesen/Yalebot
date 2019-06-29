161a474b954eff03fbfd3cd0d397e6508622a269
ErikBoesen <me@erikboesen.com>
2019-06-29 14:15:39 -0400

    Get rid of from_groupme

--- utils.py
+++ utils.py
@@ -11,35 +11,25 @@ class SenderType(Enum):
 
 
 class Message:
-    def __init__(self, raw={}, text=None, user_id=None, time=None, name=None, sender_type=SenderType.USER, group_id=None, avatar_url=None):
-        self.raw = raw
-        self.text = text or raw.get(
-        self.user_id = user_id
+    def __init__(self, raw={}, text=None):
+        self.raw = raw or {"attachments": []}
+        self.text = text or raw.get("text")
+        self.user_id = raw.get("user_id")
         time = raw.get("created_at", datetime.now())
-        elif type(time) == int:
+        if type(time) == int:
             self.time = datetime.fromtimestamp(time)
         else:
             self.time = time
-        self.name = name or "Test"
-        self.sender_type = SenderType(message.get("sender_type"))
-        self.group_id = group_id
-        self.avatar_url = avatar_url
+        self.name = raw.get("name", "Test")
+        self.sender_type = SenderType(raw.get("sender_type", "user"))
+        self.group_id = raw.get("group_id", "49940116")
+        self.avatar_url = raw.get("avatar_url", "https://i.groupme.com/900x620.jpeg.bdc7a3233afc4832a3bce3fae95c2d8b.preview")
         print(self)
 
     def __repr__(self):
         return "{location} | {name}: {text}".format(location=self.group_id,
                                                     name=self.name,
                                                     text=self.text)
-
-    @classmethod
-    def from_groupme(cls, message: dict = {"attachments": []}):
-        return cls(message,
-                   text=message.get("text"),
-                   user_id=message.get("user_id"),
-                   time=,
-                   sender_type=,
-                   group_id=message.get("group_id", "49940116"),
-                   avatar_url=message.get("avatar_url", "https://i.groupme.com/900x620.jpeg.bdc7a3233afc4832a3bce3fae95c2d8b.preview"))
 
     @property
     def image_url(self):
