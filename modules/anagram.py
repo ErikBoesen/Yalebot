32e659e41277ab72dd950c271377fa26de9d7cd3
ErikBoesen <me@erikboesen.com>
2019-07-20 19:40:03 -0400

    Complete anagram module

--- modules/anagram.py
+++ modules/anagram.py
@@ -1,6 +1,7 @@
 from .base import Module
 import requests
 from bs4 import BeautifulSoup
+import random
 
 
 class Anagram(Module):
@@ -15,5 +16,12 @@ class Anagram(Module):
                                                                                   "short": 3,
                                                                                   "cap": 1}).text
         bs = BeautifulSoup(html, "html.parser")
-        paragraph = bs.find("p", recursive=False)
-        print(paragraph)
+        paragraph = str(bs.body.find("p", recursive=False))
+        entries = paragraph.split("<br/>")
+        # Remove informational content at the bottom of the paragraph
+        entries.pop()
+        # Remove opening <p> tag
+        entries[0] = entries[0].replace("<p>", "")
+        entries = [entry.strip() for entry in entries if not any([c in entry for c in ("-", ".")])]
+        random.shuffle(entries)
+        return ", ".join(entries)
