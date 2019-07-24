a5d5ebfe94c17b35e73600bea6efc189eb767099
ErikBoesen <me@erikboesen.com>
2019-07-24 19:22:28 -0400

    Add ImageModule docstrings

--- modules/base.py
+++ modules/base.py
@@ -32,9 +32,15 @@ class Module:
         print("Loaded module %s." % self.__class__.__name__)
 
     def wave(self):
+        """
+        Generate a wave emoji with a random skin color.
+        """
         return "👋" + random.choice("🏻🏼🏽🏾🏿")
 
     def lines(self, query):
+        """
+        Split input into each individual line, for multiple-argument commands.
+        """
         return [line for line in query.split("\n") if line != ""]
 
     def bullet_list(self, pairs, embellish_first=False) -> str:
@@ -91,6 +97,9 @@ class ImageModule(Module):
         return r.json()["payload"]["url"]
 
     def rotate_upright(self, image: Image):
+        """
+        Rotate a PIL image to upright depending on its current orientation according to EXIF data.
+        """
         for orientation in ExifTags.TAGS.keys():
             if ExifTags.TAGS[orientation] == "Orientation":
                 break
@@ -105,11 +114,19 @@ class ImageModule(Module):
         return image
 
     def upload_pil_image(self, image: Image):
+        """
+        Given a PIL image, convert it into a format that can be uploaded to GroupMe's image API, then do that.
+        """
         output = BytesIO()
         image.save(output, format="JPEG", mode="RGB")
         return self.upload_image(output.getvalue())
 
     def pil_from_url(self, url):
+        """
+        Download and process an image from a given URL for later manipulation.
+        :param url: URL of image to download.
+        :return: PIL image for later manipulation.
+        """
         response = requests.get(url, stream=True)
         response.raw.decode_content = True
         image = Image.open(response.raw)
@@ -119,18 +136,38 @@ class ImageModule(Module):
         return image
 
     def resize(self, image: Image, width):
+        """
+        Resize a PIL image down to a some maximum width.
+        Useful for limiting image size.
+        :param image: PIL image to resize.
+        :param width: maximum width.
+        :return: resized image.
+        """
         natural_width, natural_height = image.size
         height = int(width * natural_height / natural_width)
         image = image.resize((width, height), Image.ANTIALIAS)
         return image
 
     def limit_image_size(self, image: Image, max_width=1000):
+        """
+        Limit an image's size in its largest dimension.
+        Will do nothing if image is already small enough.
+        :param image: PIL image to scale.
+        :param max_width: largest size to allow image to be.
+        :return: resized image.
+        """
         natural_width, natural_height = image.size
         if natural_width > max_width:
             image = self.resize(image, max_width)
         return image
 
     def get_portrait(self, user_id, group_id):
+        """
+        Get a given user's portrait in a given group.
+        :param user_id: ID of user.
+        :param group_id: ID of group that they use the portrait in.
+        :reutrn: URL of their portrait.
+        """
         # TODO: Figure out a way to not get entire list of members to find one
         members = requests.get(f"https://api.groupme.com/v3/groups/{group_id}?token={self.ACCESS_TOKEN}").json()["response"]["members"]
         for member in members:
@@ -141,6 +178,8 @@ class ImageModule(Module):
         """
         Given complete image data, extract the URL of the best image to use for a command.
         First choose attached image, then use mentioned person's avatar, then sender's avatar.
+        :param message: data of message to extract URL from.
+        :param include_avatar: should we use the avatar? Sometimes this may be undesired if another default is desired.
         :return: URL of image to use.
         """
         mention_attachments = [attachment for attachment in message.raw["attachments"] if attachment["type"] == "mentions"]
