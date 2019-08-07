5c4631637619163859bd1a74209e71a6e77e93ce
ErikBoesen <me@erikboesen.com>
2019-08-07 09:14:51 -0400

    Add more cry options

--- modules/cry.py
+++ modules/cry.py
@@ -15,7 +15,7 @@ class Cry(Module):
         "library", "buttery", "courtyard", "roof", "bench", "statue of a dead white man", "statue",
     )
     OF = (
-        "of", "at",
+        "at",
     )
     LOCATIONS = [
         "the Yale Bowl",
@@ -30,15 +30,17 @@ class Cry(Module):
         "Bass Library",
         "Skull and Key Tomb",
         "the Yale Peabody Museum of Natural History",
+        "the Yale Admissions Office",
     ] + [college + " residential college" for college in Module.COLLEGES]
     WITH = (
         "your froco", "your dus", "your suitemates", "the entire class of 2023", "yourself", "your roommate",
+        "the Skull & Bones club", "the staff of the Yale Office of Career Strategy", "the Yale Admissions staff",
+        "President Peter Salovey", "Yale College Dean Marvin Chun",
     )
     SENSITIVITY = True
 
     def response(self, query, message):
-        text = ("Cry " + random.choice(self.PREPOSITIONS) + " " + random.choice(self.ARTICLES) +
-                " " + random.choice(self.SUBLOCATIONS).upper() + " of " + random.choice(self.LOCATIONS).upper() + ".")
+        text = ("Cry " + random.choice(self.PREPOSITIONS) + " " + random.choice(self.ARTICLES) + " " + random.choice(self.SUBLOCATIONS).upper() + " of " + random.choice(self.LOCATIONS).upper() + " with " + random.choice(self.WITH).upper())
         if self.SENSITIVITY:
-            text += "\n\nIf you believe you are seriously in need of emotional support of any kind, please stop playing with this bot and seek assistance thorugh Yale Health's Mental Health & Counseling services. Call 203-432-0290 or visit https://yalehealth.yale.edu/directory/departments/mental-health-counseling. 💙"
+            text += "\n\nPlease, if you are seriously in need of help, contact Yale Mental Health & Counseling. Call 203-432-0290 or visit yalewell.yale.edu/mental-health. 💙"
         return text
