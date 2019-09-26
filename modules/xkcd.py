a8379ce1197113c5d1e42873890e74011c28f8e5
ErikBoesen <me@erikboesen.com>
2019-09-25 20:40:44 -0400

    Use HTTPS for xkcd

--- modules/xkcd.py
+++ modules/xkcd.py
@@ -6,6 +6,6 @@ class XKCD(Module):
     DESCRIPTION = "View the most recent XKCD comic, or specify a comic by number"
 
     def response(self, query, message):
-        comic = requests.get(f"http://xkcd.com/{query}/info.0.json" if query else "http://xkcd.com/info.0.json").json()
+        comic = requests.get(f"https://xkcd.com/{query}/info.0.json" if query else "https://xkcd.com/info.0.json").json()
         return ["\"" + comic["alt"] + "\"",
                 comic["img"]]
