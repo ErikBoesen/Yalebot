adc345e8125de81204d88a81f9c196e1748fef26
ErikBoesen <me@erikboesen.com>
2020-03-26 23:11:46 -0400

    Add new groups

--- modules/groups.py
+++ modules/groups.py
@@ -4,6 +4,9 @@ from .base import Module
 class Groups(Module):
     DESCRIPTION = "Get links to various Yale GroupMe groups"
     links = {
+        "LGBT": "57444490/iwNiIjzV",
+        "Also LGBT": "57668130/3G1UEdSC",
+        "Yalecraft??": "57444490/iwNiIjzV",
     }
 
     def response(self, query, message):
