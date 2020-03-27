1ae82f75f863e12494b52256992b301bcb34d8f6
ErikBoesen <me@erikboesen.com>
2020-03-26 23:26:01 -0400

    Add to groups

--- modules/groups.py
+++ modules/groups.py
@@ -5,8 +5,10 @@ class Groups(Module):
     DESCRIPTION = "Get links to various Yale GroupMe groups"
     links = {
         "California": "58985981/AC4xwNR3",
+        "Computer Science": "58985302/ZsAYNq7R",
         "LGBT": "57668130/3G1UEdSC",
         "Yalecraft??": "57444490/iwNiIjzV",
+        "Hot climate chat": "58986052/bkazz6KU",
     }
 
     def response(self, query, message):
