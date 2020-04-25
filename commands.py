21ccf14df6c229cee6c090157bc47109d4c36ab8
JoseBetancourt5 <46613756+JoseBetancourt5@users.noreply.github.com>
2020-04-25 00:08:54 -0400

    Update commands.py

--- commands.py
+++ commands.py
@@ -4,8 +4,6 @@ static_commands = {
     "ping": "Pong!",
     "testing": "Join the Yalebot testing server: https://groupme.com/join_group/49940116/f2x20kxx",
     "add": "Add me to your own group here: https://yalebot.herokuapp.com",
-    "ap": ("", "open("resources/ap.txt","r").read()"),
-
     "sam": "❗❗❗N O 💪 F L E X 💪 Z O N E ❗❗❗",
     "test": "https://erikboesen.com/yalepuritytest",
     "shrug": r"¯\_(ツ)_/¯",
