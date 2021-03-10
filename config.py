8d0f9834e4fcf7e2ff73b6e1347cca79722b94ca
ErikBoesen <me@erikboesen.com>
2021-03-10 10:09:43 -0500

    Fall back to SQLite locally

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL",
+                                             "sqlite:///" + os.path.join(basedir, "app.db"))
     # Suppress warnings
     SQLALCHEMY_TRACK_MODIFICATIONS = False
