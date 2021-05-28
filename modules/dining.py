0094fee04b30f55ac9a1f712220ed42527f7e029
ErikBoesen <me@erikboesen.com>
2021-05-28 19:14:14 -0400

    Rewrite dining module to use YaleDine API

--- modules/dining.py
+++ modules/dining.py
@@ -1,47 +1,23 @@
 from .base import Module
-from yaledining import YaleDining
+from yaledining import API
 
 
 class Dining(Module):
     DESCRIPTION = "Get information about Yale Dining services, closures, menus, etc."
-    api = YaleDining()
-    type_emoji = {
-        "Residential": "🏠",
-        "Retail": "💰",
-    }
+    api = API()
 
-    def capacity_bar(self, capacity: int):
-        return ('▇' * capacity) + ('░' * (10 - capacity))
+    def occupancy_bar(self, occupancy: int):
+        return ('▇' * occupancy) + ('░' * (10 - occupancy))
 
     def response(self, query, message):
         response = ""
-        if query:
-            location = self.api.location(query)
-            if location is None:
-                return f"Unknown location name '{query}'."
-            response = self.bullet_list(((location.name, "Open" if location.is_open else "Closed"),
-                                         ("Capacity", (f"{location.percent_capacity}% " + self.capacity_bar(location.capacity)) if location.is_open else None),
-                                         ("Type", location.type + " " + self.type_emoji[location.type]),
-                                         ("Address", f"{location.address} ({location.geolocation})"),
-                                         ("Phone", location.phone),
-                                         ("Managers", ", ".join([f"{manager.name} ({manager.email})" for manager in location.managers]))),
-                                        embellish_first=True) + "\n"
-            meals = location.meals
-            if meals:
-                response += self.bullet_list(tuple([(meal.date.strftime("%m/%d") + " " + meal.name,
-                                                     ", ".join([item.name for item in meal.items])) for meal in meals]))
-                for meal in meals:
-                    response
-            else:
-                response += "No menu is currently listed."
+        halls = self.api.halls()
+        halls = [hall for hall in halls if hall.open]
+        if halls:
+            for hall in halls:
+                response += "- {name} ({status})\n".format(name=hall.name,
+                                                                   status=("{occupancy}% occupancy ".format(occupancy=hall.occupancy * 10) + self.occupancy_bar(hall.occupancy)) if hall.occupancy is not None else "Open")
         else:
-            locations = self.api.locations()
-            locations = [location for location in locations if location.is_open]
-            if locations:
-                for location in locations:
-                    response += "- {emoji} {name} ({status})\n".format(emoji=self.type_emoji[location.type],
-                                                                       name=location.name,
-                                                                       status=("{capacity}% capacity ".format(capacity=location.percent_capacity) + self.capacity_bar(location.capacity)) if location.capacity is not None else "Open")
-            else:
-                response = "No dining locations are currently open."
+            response = "No dining halls are currently open."
+        response += "\nDownload Yale Menus at https://yalemenus.com to view full menus."
         return response
