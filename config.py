3114341f553005b458594faaab1de4f3ecde5c9e
Erik Boesen <ErikBoesen@users.noreply.github.com>
2021-09-05 17:23:42 -0400

    Fix postgres URLs

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL",
-                                             "sqlite:///" + os.path.join(basedir, "app.db"))
+                                             "sqlite:///" + os.path.join(basedir, "app.db")).replace("postgres://", "postgresql://")
     # Suppress warnings
     SQLALCHEMY_TRACK_MODIFICATIONS = False
